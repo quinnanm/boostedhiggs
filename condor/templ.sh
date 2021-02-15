b5bc13ea5b690d4d72cce48053ac96e082d94819
Cristina <criss.ms7@gmail.com>
2021-02-15 10:31:55 -0600

    test condor

--- condor/templ.sh
+++ condor/templ.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-#get setup coffea area
-xrdcp root://cmseos.fnal.gov//store/user/cmantill/coffeaenv.tar.gz ./coffeaenv.tar.gz
+# get setup coffea area
+rm *.tar.gz
+xrdcp root://cmseos.fnal.gov//store/user/cmantill/coffeaenv.tar.gz .
 tar -zxf coffeaenv.tar.gz
 source coffeaenv/bin/activate
-export PYTHONPATH=${PWD}:${PYTHONPATH}
 
+# print versions
 python -c "import awkward; print(awkward.__version__)"
+python -c "import coffea; print(coffea.__version__)"
 
 # copy code
 xrdcp -f root://cmseos.fnal.gov//store/user/cmantill/boostedhiggs.tar.gz ./boostedhiggs.tar.gz
@@ -16,5 +18,9 @@ tar -zxvf boostedhiggs.tar.gz
 cd boostedhiggs/
 python SCRIPTNAME --year YEAR --starti STARTNUM --endi ENDNUM --samples SAMPLE 
 
-#move output to eos
+# move output to eos
 xrdcp -f hhbbww.coffea EOSOUT
+
+# remove stuff
+rm *.tar.gz
+rm *.coffea
