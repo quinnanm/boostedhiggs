a9af3fd7e80d98a935edd92910c3aba1d041920c
daniel ocampo <daniel.ocampoh@udea.edu.co>
2021-07-14 19:15:08 -0500

    adding electron and muon isolation

--- binder/hwwsignal_processor.py
+++ binder/hwwsignal_processor.py
@@ -29,40 +29,56 @@ def match_HWWlepqq(genparticles,candidatefj):
     return the number of matched objects (hWW*),daughters, 
     and gen flavor (enuqq, munuqq, taunuqq) 
     """
+    # all the higgs bosons in the event (pdgID=25)
     higgs = getParticles(genparticles,25)
+    
+    # select our higgs to be all WW decays
     is_hWW = ak.all(abs(higgs.children.pdgId)==24,axis=2)
-
     higgs = higgs[is_hWW]
+    
+    # select higgs's children
     higgs_wstar = higgs.children[ak.argmin(higgs.children.mass,axis=2,keepdims=True)]
     higgs_w = higgs.children[ak.argmax(higgs.children.mass,axis=2,keepdims=True)]
     
+    # select electrons, muons and taus
     prompt_electron = getParticles(genparticles,11,11,['isPrompt','isLastCopy'])
     prompt_muon = getParticles(genparticles,13,13,['isPrompt', 'isLastCopy'])
     prompt_tau = getParticles(genparticles,15,15,['isPrompt', 'isLastCopy'])
+    
+    # choosing the quarks that not only are coming from a hard process (e.g. the WW decay)
+    # but also the ones whose parent is a W (pdgId=24), this avoids select quarks whose parent is a gluon 
+    # who also happened to be produced in association with the Higgs
     prompt_q = getParticles(genparticles,0,5,['fromHardProcess', 'isLastCopy'])
     prompt_q = prompt_q[abs(prompt_q.distinctParent.pdgId) == 24]
     
+    # counting the number of gen particles 
+    n_electrons = ak.sum(prompt_electron.pt>0,axis=1)
+    n_muons = ak.sum(prompt_muon.pt>0,axis=1)
+    n_taus = ak.sum(prompt_tau.pt>0,axis=1)
+    n_quarks = ak.sum(prompt_q.pt>0,axis=1)
+    
+    # we define the `flavor` of the Higgs decay
+    # 4(elenuqq),6(munuqq),8(taunuqq)
+    hWWlepqq_flavor = (n_quarks==2)*1 + (n_electrons==1)*3 + (n_muons==1)*5 + (n_taus==1)*7
+
+    # since our jet has a cone size of 0.8, we use 0.8 as a dR threshold
+    matchedH = candidatefj.nearest(higgs, axis=1, threshold=0.8)
+    matchedW = candidatefj.nearest(higgs_w, axis=1, threshold=0.8)
+    matchedWstar = candidatefj.nearest(higgs_wstar, axis=1, threshold=0.8) 
+
+    # matched objects
+    # 1 (H only), 4(W), 6(W star), 9(H, W and Wstar)
+    hWWlepqq_matched = (ak.sum(matchedH.pt>0,axis=1)==1)*1 + (ak.sum(ak.flatten(matchedW.pt>0,axis=2),axis=1)==1)*3 + (ak.sum(ak.flatten(matchedWstar.pt>0,axis=2),axis=1)==1)*5    
+    
+    # let's concatenate all the daughters
     dr_fj_quarks = candidatefj.delta_r(prompt_q)
     dr_fj_electrons = candidatefj.delta_r(prompt_electron)
     dr_fj_muons = candidatefj.delta_r(prompt_muon)
     dr_fj_taus = candidatefj.delta_r(prompt_tau)
     dr_daughters = ak.concatenate([dr_fj_quarks,dr_fj_electrons,dr_fj_muons,dr_fj_taus],axis=1)
-    hWWlepqq_nprongs = ak.sum(dr_daughters<0.8,axis=1)
     
-    n_electrons = ak.sum(prompt_electron.pt>0,axis=1)
-    n_muons = ak.sum(prompt_muon.pt>0,axis=1)
-    n_taus = ak.sum(prompt_tau.pt>0,axis=1)
-    n_quarks = ak.sum(prompt_q.pt>0,axis=1)
-
-    # 4(elenuqq),6(munuqq),8(taunuqq)
-    hWWlepqq_flavor = (n_quarks==2)*1 + (n_electrons==1)*3 + (n_muons==1)*5 + (n_taus==1)*7
-
-    matchedH = candidatefj.nearest(higgs, axis=1, threshold=0.8)
-    matchedW = candidatefj.nearest(higgs_w, axis=1, threshold=0.8)
-    matchedWstar = candidatefj.nearest(higgs_wstar, axis=1, threshold=0.8) 
-
-    # 1 (H only), 4(W), 6(W star), 9(H, W and Wstar)
-    hWWlepqq_matched = (ak.sum(matchedH.pt>0,axis=1)==1)*1 + (ak.sum(ak.flatten(matchedW.pt>0,axis=2),axis=1)==1)*3 + (ak.sum(ak.flatten(matchedWstar.pt>0,axis=2),axis=1)==1)*5    
+    #  number of visible daughters
+    hWWlepqq_nprongs = ak.sum(dr_daughters<0.8,axis=1)
     
     return hWWlepqq_flavor,hWWlepqq_matched,hWWlepqq_nprongs
 
@@ -70,15 +86,21 @@ class HwwSignalProcessor(processor.ProcessorABC):
     def __init__(self,jet_arbitration='pt'):
         self._jet_arbitration = jet_arbitration
         
-        # define an output
+        # output
         self.make_output = lambda: {
             'sumw': 0.,
             'signal_kin': hist2.Hist(
                 hist2.axis.IntCategory([0, 2, 4, 6, 8], name='genflavor', label='gen flavor'),
                 hist2.axis.IntCategory([0, 1, 4, 6, 9], name='genHflavor', label='higgs matching'),
                 hist2.axis.Regular(100, 200, 1200, name='pt', label=r'Jet $p_T$'),
-                #hist2.axis.Regular(4, 0, 4, name='nprongs', label='Jet n prongs'),
                 hist2.axis.IntCategory([0, 1, 2, 3, 4], name='nprongs', label='Jet n prongs'),
+                hist2.storage.Weight(),
+            ),
+            "signal_iso": hist2.Hist(
+                hist2.axis.Regular(25, 0, 1, name="eleminiIso", label="$e$ miniIso"),
+                hist2.axis.Regular(25, 0, 1, name="elerelIso", label="$e$ Rel Iso"),
+                hist2.axis.Regular(25, 0, 1, name="muminiIso", label="$\mu$ miniIso"),
+                hist2.axis.Regular(25, 0, 1, name="murelIso", label="$\mu$ Rel Iso"),
                 hist2.storage.Weight(),
             )
         }
@@ -172,12 +194,19 @@ class HwwSignalProcessor(processor.ProcessorABC):
         
         # here we fill our histogram
         output['signal_kin'].fill(
-                    genflavor=normalize(hWWlepqq_flavor, dr_lep_jet_cut),
-                    genHflavor=normalize(hWWlepqq_matched, dr_lep_jet_cut),
-                    pt = normalize(candidatefj.pt, dr_lep_jet_cut),
-                    nprongs = normalize(hWWlepqq_nprongs, dr_lep_jet_cut),
-                    weight=weights.weight()[dr_lep_jet_cut],
-                )
+        	genflavor=normalize(hWWlepqq_flavor, dr_lep_jet_cut),
+             	genHflavor=normalize(hWWlepqq_matched, dr_lep_jet_cut),
+             	pt = normalize(candidatefj.pt, dr_lep_jet_cut),
+             	nprongs = normalize(hWWlepqq_nprongs, dr_lep_jet_cut),
+              	weight=weights.weight()[dr_lep_jet_cut],
+        )
+        output['signal_iso'].fill(
+        	eleminiIso = normalize(ele_miniIso, dr_lep_jet_cut),
+            	elerelIso = normalize(ele_relIso, dr_lep_jet_cut),
+            	muminiIso = normalize(mu_miniIso,dr_lep_jet_cut),
+            	murelIso = normalize(mu_relIso,dr_lep_jet_cut),
+            	weight=weights.weight()[dr_lep_jet_cut],
+        )
 
         return {dataset: output}
             
@@ -332,3 +361,88 @@ for arbitration in ["pt", "met", "lep"]:
     )
     ax.legend(title="nprongs[HWW*]") 
     fig.savefig(f"nprongs[HWW]vspt_{arbitration}.png")
+    
+    # isolation
+    fig, ax = plt.subplots(
+        figsize=(8,7), 
+        constrained_layout=True
+    )
+
+    out["HWW"]["signal_iso"][{"murelIso":sum, "elerelIso":sum, "eleminiIso":sum}].plot1d(ax=ax)
+
+    ax.set(
+        title=title,
+        ylabel="Events"
+    )
+    plt.savefig(f"hww_muminiIso_{arbitration}.png")
+    
+    fig, ax = plt.subplots(
+        figsize=(8,7), 
+        constrained_layout=True
+    )
+
+    out["HWW"]["signal_iso"][{"muminiIso":sum,"elerelIso":sum, "eleminiIso":sum}].plot1d(ax=ax)
+
+    ax.set(
+        title=title,
+        ylabel="Events"
+    )
+    plt.savefig(f"hww_murelIso_{arbitration}.png")
+                
+    fig, ax = plt.subplots(
+        figsize=(8,7), 
+        constrained_layout=True
+    )
+
+    out["HWW"]["signal_iso"][{"murelIso":sum,"elerelIso":sum, "eleminiIso":sum}].plot1d(ax=ax, label="mini")
+    out["HWW"]["signal_iso"][{"muminiIso":sum,"elerelIso":sum, "eleminiIso":sum}].plot1d(ax=ax, label="rel")
+
+    ax.set(
+        title=title,
+        ylabel="Events",
+        xlabel="$\mu$ Isolation",
+    )
+    ax.legend(title="Isolation")
+    plt.savefig(f"hww_muIso_{arbitration}.png")
+    
+    fig, ax = plt.subplots(
+        figsize=(8,7), 
+        constrained_layout=True
+    )
+
+    out["HWW"]["signal_iso"][{"elerelIso":sum,"murelIso":sum, "muminiIso":sum}].plot1d(ax=ax)
+
+    ax.set(
+        title=title,
+        ylabel="Events"
+    )
+    plt.savefig(f"hww_eleminiIso_{arbitration}.png")
+                
+    fig, ax = plt.subplots(
+        figsize=(8,7), 
+        constrained_layout=True
+    )
+
+    out["HWW"]["signal_iso"][{"eleminiIso":sum,"murelIso":sum, "muminiIso":sum}].plot1d(ax=ax)
+
+    ax.set(
+        title=title,
+        ylabel="Events"
+    )
+    plt.savefig(f"hww_elerelIso_{arbitration}.png")
+    
+    fig, ax = plt.subplots(
+        figsize=(8,7), 
+        constrained_layout=True
+    )
+
+    out["HWW"]["signal_iso"][{"elerelIso":sum,"murelIso":sum, "muminiIso":sum}].plot1d(ax=ax, label="mini")
+    out["HWW"]["signal_iso"][{"eleminiIso":sum,"murelIso":sum, "muminiIso":sum}].plot1d(ax=ax, label="rel")
+
+    ax.set(
+        title=title,
+        ylabel="Events",
+        xlabel="$e$ Isolation",
+    )
+    ax.legend(title="Isolation")
+    plt.savefig(f"hww_eleIso_{arbitration}.png")
