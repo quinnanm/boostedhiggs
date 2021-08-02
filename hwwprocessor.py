cb384d30795d6d6ca368412379e8465b63da3b3e
daniel ocampo <daniel.ocampoh@udea.edu.co>
2021-08-02 07:13:21 -0500

    applying softdrop mass corrections

--- hwwprocessor.py
+++ hwwprocessor.py
@@ -6,7 +6,33 @@ from coffea import processor
 from coffea.nanoevents.methods import candidate, vector
 from coffea.analysis_tools import Weights, PackedSelection
 
+from coffea.lookup_tools.lookup_base import lookup_base
+from coffea import lookup_tools
+from coffea import util
 
+class SoftDropWeight(lookup_base):
+    def _evaluate(self, pt, eta):
+        gpar = np.array([1.00626, -1.06161, 0.0799900, 1.20454])
+        cpar = np.array([1.09302, -0.000150068, 3.44866e-07, -2.68100e-10, 8.67440e-14, -1.00114e-17])
+        fpar = np.array([1.27212, -0.000571640, 8.37289e-07, -5.20433e-10, 1.45375e-13, -1.50389e-17])
+        genw = gpar[0] + gpar[1]*np.power(pt*gpar[2], -gpar[3])
+        ptpow = np.power.outer(pt, np.arange(cpar.size))
+        cenweight = np.dot(ptpow, cpar)
+        forweight = np.dot(ptpow, fpar)
+        weight = np.where(np.abs(eta) < 1.3, cenweight, forweight)
+        return genw*weight
+
+
+_softdrop_weight = SoftDropWeight()
+
+
+def corrected_msoftdrop(fatjets):
+    sf = _softdrop_weight(fatjets.pt, fatjets.eta)
+    sf = np.maximum(1e-5, sf)
+    dazsle_msd = (fatjets.subjets * (1 - fatjets.subjets.rawFactor)).sum()
+    return dazsle_msd.mass * sf
+    
+    
 def getParticles(genparticles,lowid=22,highid=25,flags=['fromHardProcess', 'isLastCopy']):
     """
     returns the particle objects that satisfy a low id, 
@@ -109,24 +135,19 @@ class HwwSignalProcessor(processor.ProcessorABC):
         # output
         self.make_output = lambda: {
             'sumw': 0.,
-            "cutflow_hadel": hist2.Hist(
-                hist2.axis.StrCategory([], name='region', growth=True),
-                hist2.axis.IntCategory([0, 1, 2], name='cut', label='Cut index', growth=True),
-                hist2.axis.Regular(100, 0, 4, name="nelectrons"),
-                hist2.storage.Weight(),
-            ),
-            "cutflow_hadmu": hist2.Hist(
-                hist2.axis.StrCategory([], name='region', growth=True),
-                hist2.axis.IntCategory([0, 1, 2], name='cut', label='Cut index', growth=True),
-                hist2.axis.Regular(100, 0, 4, name="nmuons"),
-                hist2.storage.Weight(),
-            ),
             'signal_kin': hist2.Hist(
                 hist2.axis.StrCategory([], name="region", growth=True),
                 hist2.axis.IntCategory([0, 2, 4, 6, 8], name='genflavor', label='gen flavor'),
                 hist2.axis.IntCategory([0, 1, 4, 6, 9], name='genHflavor', label='higgs matching'),
-                hist2.axis.Regular(100, 200, 1200, name='pt', label=r'Jet $p_T$ [GeV]'),
                 hist2.axis.IntCategory([0, 1, 2, 3, 4], name='nprongs', label='Jet nprongs'),
+                hist2.storage.Weight(),
+            ),
+            "jet_kin": hist2.Hist(
+                hist2.axis.StrCategory([], name='region', growth=True),
+                hist2.axis.Regular(30, 200, 1000, name='jetpt', label=r'Jet $p_T$ [GeV]'),
+                hist2.axis.Regular(30, 15, 200, name="jetmsd", label="Jet $m_{sd}$ [GeV]"),
+                hist2.axis.Regular(25, -20, 0, name="jetrho", label=r"Jet $\rho$"),
+                hist2.axis.Regular(20, 0, 1, name="btag", label="Jet btag (opphem)"),
                 hist2.storage.Weight(),
             ),
             "lep_kin": hist2.Hist(
@@ -146,7 +167,12 @@ class HwwSignalProcessor(processor.ProcessorABC):
                     label=r'genH $p_T$ [GeV]',
                 ),
                 hist2.storage.Weight(),
-            )
+            ),
+            "met_kin": hist2.Hist(
+                hist2.axis.StrCategory([], name="region", growth=True),
+                hist2.axis.Regular(30, 0, 500, name="met", label=r"$p_T^{miss}$ [GeV]"),
+                hist2.storage.Weight(),
+            ),
         }
         
     def process(self, events):
@@ -253,12 +279,14 @@ class HwwSignalProcessor(processor.ProcessorABC):
         # missing transverse energy
         met = events.MET
 
-        # jets
+        # fatjets
         fatjets = events.FatJet
+        fatjets["msdcorr"] = corrected_msoftdrop(fatjets)
+        fatjets["qcdrho"] = 2 * np.log(fatjets.msdcorr / fatjets.pt)
+        
         candidatefj = fatjets[
             (fatjets.pt > 200)
         ]
-
         dphi_met_fj = abs(candidatefj.delta_phi(met))
         dr_lep_fj = candidatefj.delta_r(candidatelep_p4)
 
@@ -270,11 +298,27 @@ class HwwSignalProcessor(processor.ProcessorABC):
             candidatefj = ak.firsts(candidatefj[ak.argmin(dr_lep_fj,axis=1,keepdims=True)])
         else:
             raise RuntimeError("Unknown candidate jet arbitration")
+            
                 
         # leptons within fj
         lep_in_fj = candidatefj.delta_r(candidatelep_p4) < 0.8
         lep_in_fj = ak.fill_none(lep_in_fj, False)
         selection.add("lep_in_fj", lep_in_fj)
+        
+        # jets
+        jets = events.Jet
+        jets= jets[
+            (jets.pt > 30) 
+            & (abs(jets.eta) < 2.5) 
+            & jets.isTight
+        ][:,:4]
+        
+        dphi_jet_fj = abs(jets.delta_phi(candidatefj))
+        dr_jet_fj = abs(jets.delta_r(candidatefj))
+        
+        # b-jets
+        bjets_ophem = ak.firsts(jets[dphi_jet_fj > np.pi / 2].btagDeepFlavC)
+        #selection.add("btag_ophem", ak.max(bjets_ophem, axis=1, mask_identity=False))
 
         # match HWWlepqq 
         hWWlepqq_flavor,hWWlepqq_matched,hWWlepqq_nprongs,matchedH,genH = match_HWWlepqq(events.GenPart,candidatefj)
@@ -284,8 +328,8 @@ class HwwSignalProcessor(processor.ProcessorABC):
 
 
         regions = {
-            "hadel": ["triggere", "oneelectron"],
-            "hadmu": ["triggermu", "onemuon"] ,
+            "hadel": ["triggere", "oneelectron", "miniIso1"],
+            "hadmu": ["triggermu", "onemuon", "miniIso1"] ,
         }
 
         # function to normalize arrays after a cut or selection
@@ -296,40 +340,8 @@ class HwwSignalProcessor(processor.ProcessorABC):
             else:
                 ar = ak.to_numpy(ak.fill_none(val[cut], np.nan))
                 return ar
-                
-        for region, cuts in regions.items():
-            allcuts = set([])
-            cut = selection.all(*allcuts)
-            output["cutflow_hadel"].fill(
-                region=region,
-                cut=0,
-                nelectrons=normalize(nelectrons, None),
-                weight=weights.weight(),
-            )
-            output["cutflow_hadmu"].fill(
-                region=region,
-                cut=0,
-                nmuons=normalize(nmuons, None),
-                weight=weights.weight(),
-            )
-            for i, cut in enumerate(cuts):
-                allcuts.add(cut)
-                cut = selection.all(*allcuts)
-                print(allcuts)
-                output["cutflow_hadel"].fill(
-                    region=region,
-                    cut=i+1,
-                    nelectrons=normalize(nelectrons, cut),
-                    weight=weights.weight()[cut],
-                )
-                output["cutflow_hadmu"].fill(
-                    region=region,
-                    cut=i+1,
-                    nmuons=normalize(nmuons, cut),
-                    weight=weights.weight()[cut],
-                )
-
-        # lepton kin
+        
+        
         def fill(region):
             selections = regions[region]
             cut = selection.all(*selections)
@@ -338,9 +350,16 @@ class HwwSignalProcessor(processor.ProcessorABC):
                 region=region,
                 genflavor=normalize(hWWlepqq_flavor, cut),
                 genHflavor=normalize(hWWlepqq_matched, cut),
-                pt=normalize(candidatefj.pt, cut),
                 nprongs=normalize(hWWlepqq_nprongs, cut),
                 weight = weights.weight()[cut],
+            )
+            output["jet_kin"].fill(
+                region=region,
+                jetpt=normalize(candidatefj.pt, cut),
+                jetmsd=normalize(candidatefj.msdcorr, cut),
+                jetrho=normalize(candidatefj.qcdrho, cut),
+                btag=normalize(bjets_ophem, cut),
+                weight=weights.weight()[cut],
             )
             output['lep_kin'].fill(
                 region=region,
@@ -354,6 +373,11 @@ class HwwSignalProcessor(processor.ProcessorABC):
                 region=region,
                 matchedHpt=normalize(matchedH_pt, cut),
                 genHpt=normalize(ak.firsts(genH.pt), cut),
+                weight=weights.weight()[cut],
+            )
+            output["met_kin"].fill(
+                region=region,
+                met=normalize(met.pt, cut),
                 weight=weights.weight()[cut],
             )
 
