e54dfc7b0e26eb3a7d93eff57de813d388a28c92
Cristina <criss.ms7@gmail.com>
2021-08-17 14:13:45 -0500

    fix options

--- condor/submit.templ.sh
+++ condor/submit.templ.sh
@@ -6,7 +6,7 @@ python3 -m pip install correctionlib==2.0.0rc6
 mkdir outfiles
 
 # run code (e.g. for run.py)
-python SCRIPTNAME --year YEAR --starti STARTNUM --endi ENDNUM --samples SAMPLE --processor PROCESSOR --condor --json metadata.json 
+python SCRIPTNAME --year YEAR --starti STARTNUM --endi ENDNUM --sample SAMPLE --processor PROCESSOR --condor --fileset metadata.json 
 
 #move output to eos
 xrdcp -f outfiles/*.hist EOSOUT
