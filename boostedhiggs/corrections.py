505ffaf11a2e98ee56ef7dc641da991eaec505ef
Cristina <criss.ms7@gmail.com>
2021-08-17 10:05:59 -0500

    import w resources

--- boostedhiggs/corrections.py
+++ boostedhiggs/corrections.py
@@ -3,13 +3,11 @@ import numpy as np
 import awkward as ak
 import gzip
 import pickle
+import importlib.resources
 import correctionlib
 from coffea.lookup_tools.lookup_base import lookup_base
 from coffea import lookup_tools
 from coffea import util
-
-with gzip.open(os.path.join(os.path.dirname(__file__), 'data', 'corrections.pkl.gz')) as fin:
-    compiled = pickle.load(fin)
 
 class SoftDropWeight(lookup_base):
     def _evaluate(self, pt, eta):
@@ -33,18 +31,11 @@ def corrected_msoftdrop(fatjets):
 
 def build_lumimask(filename):
     from coffea.lumi_tools import LumiMask
-    return LumiMask(os.path.join(os.path.dirname(__file__), 'data', filename))
+    with importlib.resources.path("boostedhiggs.data", filename) as path:
+        return LumiMask(path)
 
 lumiMasks = {
     '2016': build_lumimask('Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'),
     '2017': build_lumimask('Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt'),
     '2018': build_lumimask('Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'),
 }
-
-def add_pileup_weight(weights, nPU, year='2017', dataset=None):
-    weights.add(
-        'pileup_weight',
-        compiled[f'{year}_pileupweight'](nPU),
-        compiled[f'{year}_pileupweight_puUp'](nPU),
-        compiled[f'{year}_pileupweight_puDown'](nPU),
-    )
