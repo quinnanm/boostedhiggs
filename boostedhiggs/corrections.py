644cc645b1f58acb1ef4e28e7aa27a5189315c64
Cristina <criss.ms7@gmail.com>
2021-09-07 11:30:12 -0500

    fix lumimask

--- boostedhiggs/corrections.py
+++ boostedhiggs/corrections.py
@@ -95,9 +95,9 @@ def build_lumimask(filename):
         return LumiMask(path)
 
 lumiMasks = {
-    "2016": "Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt",
-    "2017": "Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt",
-    "2018": "Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt",
+    "2016": build_lumimask("Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"),
+    "2017": build_lumimask("Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt"),
+    "2018": build_lumimask("Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt"),
 }
 
 # TODO: Update to UL
