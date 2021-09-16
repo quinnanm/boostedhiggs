f3daf164604f7594e89962f4c389397b8fc961bf
Daniel Ocampo Henao <daniel.ocampoh@udea.edu.co>
2021-09-16 08:49:52 -0500

    adding DYJets and single-t backgrounds

--- python/plot_stack.py
+++ python/plot_stack.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import mplhep as hep
 hep.style.use("CMS")
 
+import os
 import argparse
 import hist as hist2
 import _pickle as cPickle
@@ -21,47 +22,60 @@ def main(args):
     map_proc = {
         "hww": "GluGluHToWWToLNuQQ",
         "qcd": "QCD",
-        "tt": "TTTo2L2Nu",
+        "tt_semileptonic": "TTToSemiLeptonic",
+        "tt_hadronic": "TTToHadronic",
+        "tt_dileptonic": "TTTo2L2Nu",
         "wjets": "WJetsToLNu",
+        "zjets": "DYJetsToLL",
+        "st": "ST"
     }
     
     paths_by_proc = {}
     
     if args.channel=="hadmu":
-        paths_by_proc['data'] = "%s/muon_%s.pkl"%(input_dir,args.hist_name)
+        paths_by_proc['data'] = f"{input_dir}/muon_{args.hist_name}.pkl"
         data_key = "SingleMuon"
     elif args.channel=="hadel":
-        paths_by_proc['data'] = "%s/electron_%s.pkl"%(input_dir,args.hist_name)
+        paths_by_proc['data'] = f"{input_dir}/electron_{args.hist_name}.pkl"
         data_key = "SingleElectron"
     else:
         print('Not a valid data channel')
         exit
+
     map_proc['data'] = data_key
+    paths_by_proc['hww'] = f"{input_dir}/hww_{args.hist_name}.pkl"
 
-    paths_by_proc['hww'] = "%s/hww_%s.pkl"%(input_dir,args.hist_name)
-
-    mc_procs = ["tt","qcd","wjets"]
+    mc_procs = ["tt_semileptonic", "tt_hadronic", "tt_dileptonic", "qcd", "wjets", "zjets", "st"]
     for mc in mc_procs:
-        paths_by_proc[mc] = "%s/%s_%s.pkl"%(input_dir,mc,args.hist_name)
+        paths_by_proc[mc] = f"{input_dir}/{mc}_{args.hist_name}.pkl"
 
     hists_by_proc = {}
     for key,path in paths_by_proc.items():
         with open(path, "rb") as f:
             pklf = cPickle.load(f)
         hists_by_proc[key] = pklf[map_proc[key]][args.hist_name][{"region":args.channel}].project(args.hist_axis)
-        print(key,hists_by_proc[key])
+
+    print(f"generating {args.channel}_{args.hist_axis} plot")
 
     if args.hist_name == "cutflow":
         from utils import plot_cutflow
         plot_cutflow(
             data=hists_by_proc["data"],
             sig=hists_by_proc["hww"],
-            bkg=[hists_by_proc["wjets"],
-                 hists_by_proc["tt"],
-                 hists_by_proc["qcd"]],
-            bkg_labels=[r"$W(l\nu)$+jets", 
-                        r"$t\bar{t}$ semileptonic", 
-                        "QCD"],
+            bkg=[hists_by_proc["tt_hadronic"],
+                 hists_by_proc["tt_dileptonic"],
+                 hists_by_proc["st"],
+                 hists_by_proc["zjets"],
+                 hists_by_proc["tt_semileptonic"],
+                 hists_by_proc["wjets"],
+                 hists_by_proc["qcd"],],
+            bkg_labels=[r"$t\bar{t}$ hadronic",
+                        r"$t\bar{t}$ dileptonic",
+                        r"Single-t",
+                        r"$Z(ll)$",
+                        r"$t\bar{t}$ semileptonic",
+                        r"$W(l\nu)$",
+                        "QCD"], 
             region=args.channel,
             odir=args.odir,
             year=args.year,
@@ -71,13 +85,21 @@ def main(args):
         plot_stack(
             data=hists_by_proc["data"],
             sig=hists_by_proc["hww"],
-            bkg=[hists_by_proc["wjets"],
-                 hists_by_proc["tt"],
-                 hists_by_proc["qcd"]],
-            bkg_labels=[r"$W(l\nu)$+jets", 
-                        r"$t\bar{t}$ semileptonic", 
-                        "QCD"],
-            axis_name = args.hist_axis,
+            bkg=[hists_by_proc["tt_hadronic"],
+                 hists_by_proc["tt_dileptonic"],
+                 hists_by_proc["st"],
+                 hists_by_proc["zjets"],
+                 hists_by_proc["tt_semileptonic"],
+                 hists_by_proc["qcd"],
+                 hists_by_proc["wjets"]],
+            bkg_labels=[r"$t\bar{t}$ hadronic",
+                        r"$t\bar{t}$ dileptonic",
+                        r"Single-t",
+                        r"$Z(ll)$",
+                        r"$t\bar{t}$ semileptonic",
+                        "QCD",
+                        r"$W(l\nu)$"],
+            axis_name=args.hist_axis,
             region=args.channel,
             odir=args.odir,
             year=args.year,
@@ -85,16 +107,15 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--hname',      dest='hist_name',  default=None,         help="hist name", type=str, required=True)
-    parser.add_argument('--haxis',      dest='hist_axis',  default=None,         help="hist axis to project", type=str, required=True)
-    parser.add_argument('--year',       dest='year',       default=None,         help="year", type=str, required=True)
-    parser.add_argument('--channel',    dest='channel',    default=None,         help="channel (hadmu or hadel)", required=True)
-    parser.add_argument('--idir',       dest='idir',       default=None,         help="directory with pickled histograms", required=True)
-    parser.add_argument('--odir',       dest='odir',       default='plots/',     help="output directory with plots")
+    parser.add_argument('--hname',      dest='hist_name',  default=None,  help="hist name",                         type=str,   required=True)
+    parser.add_argument('--haxis',      dest='hist_axis',  default=None,  help="hist axis to project",              type=str,   required=True)
+    parser.add_argument('--year',       dest='year',       default=None,  help="year",                              type=str,   required=True)
+    parser.add_argument('--channel',    dest='channel',    default=None,  help="channel (hadmu or hadel)",          type=str,   required=True)
+    parser.add_argument('--idir',       dest='idir',       default=None,  help="directory with pickled histograms", type=str,   required=True)
+    parser.add_argument('--odir',       dest='odir',       default=None,  help="output directory with plots",       type=str,   required=True)
 
     args = parser.parse_args()
 
-    import os
-    os.system('mkdir -p %s'%args.odir)
+    os.system(f'mkdir -p {args.odir}')
 
     main(args)
