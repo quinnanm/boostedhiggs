20cc4131babc8ef1729801cadb2239d0e8b6322d
Farouk <farouk.mokhtar@gmail.com>
2022-02-04 17:28:16 -0800

    add sample

--- make_plots.py
+++ make_plots.py
@@ -121,6 +121,7 @@ def main(args):
                      # density=True
                      )
         # ax.set_ylim(0,1)
+        ax.set_title(f'{sample}')
         ax.legend()
 
         if not os.path.exists('hists'):
@@ -130,17 +131,13 @@ def main(args):
 
 
 if __name__ == "__main__":
-    # e.g. to run over the 2 parquet results of 0-1 and 1-2 do this
-    # run locally as: python make_plots.py --year 2017 --starti 0 --endi 2 --sample GluGluHToWWToLNuQQ_M125_TuneCP5_PSweight_13TeV-powheg2-jhugen727-pythia8
+    # e.g.
+    # run locally as: python make_plots.py --year 2017 --sample GluGluHToWWToLNuQQ_M125_TuneCP5_PSweight_13TeV-powheg2-jhugen727-pythia8,QCD_HT1000to1500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8
     parser = argparse.ArgumentParser()
     parser.add_argument('--year',       dest='year',       default='2017',       help="year", type=str)
-    parser.add_argument('--starti',     dest='starti',     default=0,            help="start index of files", type=int)
-    parser.add_argument('--endi',       dest='endi',       default=-1,           help="end index of files", type=int)
-    parser.add_argument("--processor",  dest="processor",  default="hww",        help="HWW processor", type=str)
-    parser.add_argument("--dask",       dest="dask",       action="store_true",  default=False, help="Run with dask")
+    # parser.add_argument('--starti',     dest='starti',     default=0,            help="start index of files", type=int)
+    # parser.add_argument('--endi',       dest='endi',       default=-1,           help="end index of files", type=int)
     parser.add_argument('--sample',     dest='sample',     default=None,         help='sample name', required=True)
-    parser.add_argument("--pfnano",     dest='pfnano',     action="store_true",  default=False, help="Run with pfnano")
-    parser.add_argument("--chunksize",  dest='chunksize',  type=int, default=2750, help="chunk size in processor")
     parser.add_argument(
         "--executor",
         type=str,
