739b36c964786ba93ebba491ec153211f385e4bc
Farouk <farouk.mokhtar@gmail.com>
2022-02-18 10:10:35 -0800

    s

--- run.py
+++ run.py
@@ -52,9 +52,9 @@ def main(args):
     with open(fname, 'r') as f:
         if args.pfnano:
             files = json.load(f)[args.year]
-            for subdir in files.keys():
-                # for key, flist in files[subdir].items():
-                for s in samples:
+            for s in samples:
+                for subdir in files.keys():
+                    # for key, flist in files[subdir].items():
                     if s in files[subdir].keys():
                         print('s', s)
                         fileset[s] = ["root://cmsxrootd.fnal.gov/" + f for f in files[subdir][key][args.starti:args.endi]]
