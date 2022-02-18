2d2ba86d405bca793d8672c3e036992b897cf672
Farouk <farouk.mokhtar@gmail.com>
2022-02-18 10:07:04 -0800

    d

--- run.py
+++ run.py
@@ -56,7 +56,9 @@ def main(args):
                 # for key, flist in files[subdir].items():
                 for s in samples:
                     if s in key:
-                        print('s', s)
+                        print('files[subdir]', files[subdir].keys())
+                        print('key', key)
+
                         fileset[s] = ["root://cmsxrootd.fnal.gov/" + f for f in files[subdir][key][args.starti:args.endi]]
         else:
             files = json.load(f)
