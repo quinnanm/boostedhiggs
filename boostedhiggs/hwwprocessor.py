60127817d91d6e1ea84ed5edc3b02877f9a1068d
Cristina <criss.ms7@gmail.com>
2022-03-09 18:57:14 -0600

    add trigger sel

--- boostedhiggs/hwwprocessor.py
+++ boostedhiggs/hwwprocessor.py
@@ -87,6 +87,8 @@ class HwwProcessor(processor.ProcessorABC):
                 "lep_nprongs",
                 "lep_iswlepton",
                 "lep_iswstarlepton",
+                "trigger_iso",
+                "trigger_noniso",
                 "weight",
             ],
             'mu': [
@@ -106,6 +108,8 @@ class HwwProcessor(processor.ProcessorABC):
                 "lep_nprongs",
                 "lep_iswlepton",
                 "lep_iswstarlepton",
+                "trigger_iso",
+                "trigger_noniso",
                 "weight",
             ],
             'had': [
@@ -288,17 +292,27 @@ class HwwProcessor(processor.ProcessorABC):
             self.cutflows[ch]["all"] = nevents
 
         # trigger
-        triggers = {}
+        trigger_noiso = {}
+        trigger_iso = {}
         for ch in self._channels:
             if ch == "had" and isMC:
                 trigger = np.ones(nevents, dtype='bool')
+                trigger_noiso[ch] = np.zeros(nevents, dtype='bool')
+                trigger_iso[ch] = np.zeros(nevents, dtype='bool')
             else:
                 # apply trigger to both data and MC (except for hadronic channel)
                 trigger = np.zeros(nevents, dtype='bool')
+                trigger_noiso[ch] = np.zeros(nevents, dtype='bool')
+                trigger_iso[ch] = np.zeros(nevents, dtype='bool')
                 for t in self._HLTs[ch]:
                     if t in events.HLT.fields:
+                        if "Iso" in t or "WPTight_Gsf" in t:
+                            trigger_iso[ch] = trigger_iso[ch] | events.HLT[t]
+                        else:
+                            trigger_noiso[ch] = trigger_noiso[ch] | events.HLT[t]
+
                         trigger = trigger | events.HLT[t]
-            self.add_selection("trigger", trigger, [ch])
+            # self.add_selection("trigger", trigger, [ch])
             del trigger
 
         # metfilters
@@ -599,6 +613,8 @@ class HwwProcessor(processor.ProcessorABC):
             for var in self._skimvars[ch]:
                 if var in variables.keys():
                     out[var] = variables[var]
+                out["trigger_iso"] = pad_val(trigger_iso[ch],-1)
+                out["trigger_noiso"] = pad_val(trigger_noiso[ch],-1)
 
             fill_output = True
             # for data, only fill output for that channel
