8aeb1c9056bfbd5a09490fadfbe1508103e59d3b
Farouk <farouk.mokhtar@gmail.com>
2022-04-04 14:40:50 -0700

    pick closer to met

--- boostedhiggs/hwwprocessor.py
+++ boostedhiggs/hwwprocessor.py
@@ -425,8 +425,6 @@ class HwwProcessor(processor.ProcessorABC):
         # good_fatjets = good_fatjets[lep_in_fj_overlap_bool]
         # candidatefj_lep = ak.firsts(good_fatjets[ak.argmin(good_fatjets.delta_r(candidatelep_p4), axis=1, keepdims=True)])      # get candidatefj for leptonic channel
 
-        candidatefj_lep = ak.firsts(good_fatjets[ak.argmin(good_fatjets.delta_r(met), axis=1, keepdims=True)])      # get candidatefj for leptonic channel
-
         # lepton and fatjet mass
         lep_fj_m = (candidatefj_lep - candidatelep_p4).mass  # mass of fatjet without lepton
 
@@ -446,6 +444,8 @@ class HwwProcessor(processor.ProcessorABC):
         mt_lep_met = np.sqrt(
             2. * candidatelep_p4.pt * met.pt * (ak.ones_like(met.pt) - np.cos(candidatelep_p4.delta_phi(met)))
         )
+
+        candidatefj_lep = ak.firsts(good_fatjets[ak.argmin(good_fatjets.delta_r(met), axis=1, keepdims=True)])      # get candidatefj for leptonic channel
 
         # event selections for muon channel
         self.add_selection(
