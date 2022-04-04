90ccfc1b9f2365ffc895ca0ed795b57a5593b071
Farouk <farouk.mokhtar@gmail.com>
2022-04-04 10:08:45 -0700

    update btag for 2016

--- boostedhiggs/btag.py
+++ boostedhiggs/btag.py
@@ -1,7 +1,7 @@
 import correctionlib
 import awkward as ak
 import numpy as np
-import pickle as pkl 
+import pickle as pkl
 import importlib.resources
 
 from coffea.lookup_tools.correctionlib_wrapper import correctionlib_wrapper
@@ -10,7 +10,7 @@ from coffea.lookup_tools.dense_lookup import dense_lookup
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation
 btagWPs = {
     "deepJet": {
-        '2016preVFP': {
+        '2016': {  # this one is preVFP
             'L': 0.0508,
             'M': 0.2598,
             'T': 0.6502,
@@ -37,19 +37,20 @@ taggerBranch = {
     "deepCSV": "btagDeep"
 }
 
+
 class BTagCorrector:
     def __init__(self, wp, tagger="deepJet", year="2017", mod=""):
-        self._year = year+mod
+        self._year = year + mod
         self._tagger = tagger
         self._wp = wp
-        self._btagwp = btagWPs[tagger][year+mod][wp]
+        self._btagwp = btagWPs[tagger][year + mod][wp]
         self._branch = taggerBranch[tagger]
 
         # more docs at https://cms-nanoaod-integration.web.cern.ch/commonJSONSFs/BTV_btagging_Run2_UL/BTV_btagging_201*_UL.html
         self._cset = correctionlib.CorrectionSet.from_file(f"/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/BTV/{year}_UL/btagging.json.gz")
 
         # efficiency lookup
-        with importlib.resources.path("boostedhiggs.data", f"btageff_{tagger}_{wp}_{year}.pkl") as path: 
+        with importlib.resources.path("boostedhiggs.data", f"btageff_{tagger}_{wp}_{year}.pkl") as path:
             with open(path, 'rb') as f:
                 eff = pkl.load(f)
 
@@ -59,14 +60,14 @@ class BTagCorrector:
         # syst: central, down, down_correlated, down_uncorrelated, up, up_correlated
         # until correctionlib handles jagged data natively we have to flatten and unflatten
         j, nj = ak.flatten(j), ak.num(j)
-        sf = self._cset["%s_incl"%self._tagger].evaluate(syst, self._wp, np.array(j.hadronFlavour), np.array(abs(j.eta)), np.array(j.pt))
+        sf = self._cset["%s_incl" % self._tagger].evaluate(syst, self._wp, np.array(j.hadronFlavour), np.array(abs(j.eta)), np.array(j.pt))
         return ak.unflatten(sf, nj)
-        
+
     def btagSF(self, j, syst="central"):
         # syst: central, down, down_correlated, down_uncorrelated, up, up_correlated
         # until correctionlib handles jagged data natively we have to flatten and unflatten
         j, nj = ak.flatten(j), ak.num(j)
-        sf = self._cset["%s_comb"%self._tagger].evaluate(syst, self._wp, np.array(j.hadronFlavour), np.array(abs(j.eta)), np.array(j.pt))
+        sf = self._cset["%s_comb" % self._tagger].evaluate(syst, self._wp, np.array(j.hadronFlavour), np.array(abs(j.eta)), np.array(j.pt))
         return ak.unflatten(sf, nj)
 
     def addBtagWeight(self, jets, weights):
@@ -77,7 +78,7 @@ class BTagCorrector:
         """
         lightJets = jets[jets.hadronFlavour == 0]
         bcJets = jets[jets.hadronFlavour > 0]
-        
+
         lightEff = self.efflookup(lightJets.pt, abs(lightJets.eta), lightJets.hadronFlavour)
         bcEff = self.efflookup(bcJets.pt, abs(bcJets.eta), bcJets.hadronFlavour)
 
@@ -88,9 +89,9 @@ class BTagCorrector:
             # tagged SF = SF*eff / eff = SF
             tagged_sf = ak.prod(sf[passbtag], axis=-1)
             # untagged SF = (1 - SF*eff) / (1 - eff)
-            untagged_sf = ak.prod(((1 - sf*eff) / (1 - eff))[~passbtag], axis=-1)
+            untagged_sf = ak.prod(((1 - sf * eff) / (1 - eff))[~passbtag], axis=-1)
             return tagged_sf * untagged_sf
-            
+
         lightweight = combine(
             lightEff,
             self.lighttagSF(lightJets, "central"),
@@ -101,7 +102,7 @@ class BTagCorrector:
             self.btagSF(bcJets, "central"),
             bcPass
         )
-        
+
         # nominal weight = btagSF (btagSFbc*btagSFlight)
         nominal = lightweight * bcweight
         weights.add('btagSF', lightweight * bcweight)
@@ -112,7 +113,7 @@ class BTagCorrector:
         # btagSFlight_correlated: btagSFlight_up/down_correlated
         # btagSFbc_correlated:  btagSFbc_up/down_correlated
         weights.add(
-            'btagSFlight_%s'%self._year,
+            'btagSFlight_%s' % self._year,
             np.ones(len(nominal)),
             weightUp=combine(
                 lightEff,
@@ -126,7 +127,7 @@ class BTagCorrector:
             )
         )
         weights.add(
-            'btagSFbc_%s'%self._year, 
+            'btagSFbc_%s' % self._year,
             np.ones(len(nominal)),
             weightUp=combine(
                 bcEff,
@@ -169,13 +170,14 @@ class BTagCorrector:
         )
         return nominal
 
+
 if __name__ == '__main__':
     from coffea.analysis_tools import Weights
     from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 
-    tagger="deepJet"
-    year="2018"
-    wp="M"
+    tagger = "deepJet"
+    year = "2018"
+    wp = "M"
 
     tt_files = {
         "2018": "root://cmsxrootd-site.fnal.gov//store/mc/RunIISummer20UL18NanoAODv9/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/120000/0520A050-AF68-EF43-AA5B-5AA77C74ED73.root",
@@ -218,6 +220,6 @@ if __name__ == '__main__':
 
     jets = events.Jet[phasespace_cuts]
     weights = Weights(len(events), storeIndividual=True)
-    b = BTagCorrector('M',tagger,'2018')
-    nom = b.addBtagWeight(jets,weights)
+    b = BTagCorrector('M', tagger, '2018')
+    nom = b.addBtagWeight(jets, weights)
     print(nom)
