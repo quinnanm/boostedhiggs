ad0e7e1636aeff6e0b40928bfce43fbb323d502c
Farouk <farouk.mokhtar@gmail.com>
2022-04-18 15:41:57 -0700

    up

--- python/dataframe2root.py
+++ python/dataframe2root.py
@@ -1,4 +1,4 @@
-#script for converting pandas dataframe to rootfile
+# script for converting pandas dataframe to rootfile
 #import fastparquet
 import os
 import uproot
@@ -12,43 +12,43 @@ import pyarrow.parquet as pq
 pd.options.mode.chained_assignment = None  # default='warn'
 
 parser = argparse.ArgumentParser(description='converting pandas dataframe to rootfile ')
-parser.add_argument("-p", "--proc", dest="proc", default='parquet') #specify file type like QCD
+parser.add_argument("-p", "--proc", dest="proc", default='parquet')  # specify file type like QCD
 parser.add_argument("-y", "--year", dest="year", default='2016')
 parser.add_argument("-t", "--treename", dest="treename", default='Events')
 parser.add_argument("-d", "--data", dest="isdata", default='False')
 args = parser.parse_args()
 
-indir = '/eos/uscms/store/user/fmokhtar/boostedhiggs/Apr4_'+args.year
-filetype='.parquet' 
+indir = '/eos/uscms/store/user/fmokhtar/boostedhiggs/Apr4_' + args.year
+filetype = '.parquet'
 outdir = './rootfiles/'
-print(args.proc+' must be in file')
+print(args.proc + ' must be in file')
 
 for subdir, dirs, files in os.walk(indir):
     for file in files:
-        #load files
+        # load files
         if ('had.parquet' in file) and (args.proc in subdir):
-            f=subdir+'/'+file
+            f = subdir + '/' + file
             # f=f.replace("/eos/uscms/","root://cmseos.fnal.gov//")
-            outf=f
+            outf = f
             print('prepping input file', f, '...')
-            outname = outf[outf.rfind(args.year+'/')+5:]
-            outname = outdir+outname.strip('.'+filetype)+'.root'
+            outname = outf[outf.rfind(args.year + '/') + 5:]
+            outname = outdir + outname.strip('.' + filetype) + '.root'
             outname = outname.replace('/outfiles/', '_')
-            
-            #load parquet into dataframe
+
+            # load parquet into dataframe
             print('loading dataframe...')
             table = pq.read_table(f)
             data = table.to_pandas()
-            print('# input events:', len(data)  )
-            if len(data)==0:
+            print('# input events:', len(data))
+            if len(data) == 0:
                 print('no skimmed events. skipping')
                 continue
 
-            #here is where you can add branches to the tree, skim the selection to include fewer events, etc   
+            # here is where you can add branches to the tree, skim the selection to include fewer events, etc
 
-            #fill dataframe to rootfile
+            # fill dataframe to rootfile
             # array2root(data.to_records(index=False), filename=outname, treename=args.treename, mode='RECREATE') #dont use, requires root
-            file = uproot.recreate(outname) 
-            file[args.treename]=uproot.newtree(data)
-            print('Wrote rootfile ',outname)
-
+            print(outname)
+            file = uproot.recreate(outname)
+            file[args.treename] = uproot.newtree(data)
+            print('Wrote rootfile ', outname)
