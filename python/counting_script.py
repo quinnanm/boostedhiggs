6fdc76280e32f04a4fd4f13166a757cb2e28bdac
Farouk <farouk.mokhtar@gmail.com>
2022-05-16 13:53:20 -0700

    d

--- python/counting_script.py
+++ python/counting_script.py
@@ -67,9 +67,10 @@ if __name__ == "__main__":
                     combine = True
         if not combine:
             num_dict[sample] = 0
-        print(num_dict.keys())
         # get list of parquet files that have been processed
         parquet_files = glob.glob(f'{idir}/{sample}/outfiles/*_{args.ch}.parquet')
+        print(parquet_files)
+        print('combine', combine)
 
         if len(parquet_files) == 0:
             continue
