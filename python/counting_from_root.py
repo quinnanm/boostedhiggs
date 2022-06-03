74d3e01736b6d99d812083dc95b6e86dfb4efe08
Farouk <farouk.mokhtar@gmail.com>
2022-06-03 18:08:35 +0200

    up

--- python/counting_from_root.py
+++ python/counting_from_root.py
@@ -9,19 +9,26 @@ import pickle as pkl
 
 # counts events stored in root files
 
-num_dict = {}
+counts = {}
 
 for ch in ['had', 'mu', 'ele']:
-    num_dict[ch] = {}
+    counts[ch] = {}
     print(f'For {ch} channel')
-    for sample in ['QCD', 'data', 'signal', 'signal2', 'ttbar', 'wjets', 'other']:
-        if os.path.isfile(f"/eos/uscms/store/user/mequinna/boostedhiggs/combinetest_23may22/merged/2017/{sample}_ele_merged.root"):
+
+    # repo = '/eos/uscms/store/user/mequinna/boostedhiggs/combinetest_23may22/merged/2017/'
+    repo = '/uscms/home/fmokhtar/nobackup/boostedhiggs/python/merged/2017/'
+
+    for sample in os.listdir(repo):
+
+        fname = repo + f'{sample}_{ch}_merged.root'
+
+        if os.path.isfile(f"{fname}"):
             # load in uproot
-            events = uproot.open(f"/eos/uscms/store/user/mequinna/boostedhiggs/combinetest_23may22/merged/2017/{sample}_ele_merged.root")
+            events = uproot.open(f"{fname}")
             # sum tot_weight
-            num_dict[ch][sample] = events['Events']['tot_weight'].array(library="np").sum()
+            counts[ch][sample] = events['Events']['tot_weight'].array(library="np").sum()
             print(f'number of events for {sample} is {num_dict[ch][sample]}')
     print(f'-----------------------------------------')
 
 with open(f'./counts_from_root.pkl', 'wb') as f:  # saves the hists objects
-    pkl.dump(num_dict, f)
+    pkl.dump(counts, f)
