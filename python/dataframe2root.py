a753f5b3c0462e92f7acaeeb620f54392f16ebf7
Farouk <farouk.mokhtar@gmail.com>
2022-06-03 16:07:57 +0200

    up

--- python/dataframe2root.py
+++ python/dataframe2root.py
@@ -12,65 +12,38 @@ import pyarrow.parquet as pq
 pd.options.mode.chained_assignment = None  # default='warn'
 
 parser = argparse.ArgumentParser(description='converting pandas dataframe to rootfile ')
-parser.add_argument("-p",   "--proc",     dest="proc", default='parquet')  # specify file type like QCD
-parser.add_argument("-t",   "--treename", dest="treename", default='Events')
-parser.add_argument("-d",   "--data", dest="isdata", default='False')
-parser.add_argument("-ch",  "--ch",   dest="ch", default='had')
-parser.add_argument("-dir", "--dir",  dest="dir", default='Apr20_2016')
+parser.add_argument("-p", "--process", dest="process", default='process')  # specify file type like QCD
+parser.add_argument("-y", "--year", dest="year", default='2016')
+parser.add_argument("-t", "--treename", dest="treename", default='Events')
+parser.add_argument("-d", "--data", dest="isdata", default='False')
 args = parser.parse_args()
 
+indir = '/eos/uscms/store/user/cmantill/boostedhiggs/May7_2017'
+filetype = '.parquet'
+outdir = './rootfiles/'
 
-if __name__ == "__main__":
-    """
-    e.g. run as:
-    python -u dataframe2root.py --dir='Apr20_2016' --ch='had' --proc='QCD'
-    """
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
 
-    year = args.dir[-4:]
-    indir = '/eos/uscms/store/user/fmokhtar/boostedhiggs/' + args.dir
-    filetype = '.parquet'
+print(args.process + ' must be in file')
 
-    # make directory to hold rootfiles
-    outdir = './rootfiles/'
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-    outdir = './rootfiles/' + args.proc + '/'
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-    outdir = './rootfiles/' + args.proc + '/' + args.ch + '/'
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
+for subdir, dirs, files in os.walk(indir):
+    for file in files:
+        # load files
+        if ('had.parquet' in file) and ('QCD' in subdir):
+            f = subdir + '/' + file
 
-    print(f'processing {args.ch} channel')
-    for subdir, dirs, files in os.walk(indir):
-        for file in files:
-            # load files
-            if (f'{args.ch}.parquet' in file) and (args.proc in subdir):
-                f = subdir + '/' + file
-                # f=f.replace("/eos/uscms/","root://cmseos.fnal.gov//")
-                outf = f
-                print('prepping input file', f, '...')
-                outname = outf[outf.rfind(year + '/') + 5:]
-                outname = outdir + outname.strip('.' + filetype) + '.root'
-                outname = outname.replace('/outfiles/', '_')
+            # load parquet into dataframe
+            print('loading dataframe...')
+            table = pq.read_table(f)
+            data = table.to_pandas()
+            print('# input events:', len(data))
+            if len(data) == 0:
+                print('no skimmed events. skipping')
+                continue
 
-                # load parquet into dataframe
-                print('loading dataframe...')
-                table = pq.read_table(f)
-                data = table.to_pandas()
-                print('# input events:', len(data))
-                if len(data) == 0:
-                    print('no skimmed events. skipping')
-                    continue
+            outname = outdir + file[:-8] + '.root'  # the slice removes the .parquet extension (to replace it with a .root extension)
+            with uproot.recreate(outdir + file[:-8] + '.root') as file:
+                file[args.treename] = pd.DataFrame(data)
 
-                print(data.keys())
-                # here is where you can add branches to the tree, skim the selection to include fewer events, etc
-
-                # fill dataframe to rootfile
-                # array2root(data.to_records(index=False), filename=outname, treename=args.treename, mode='RECREATE') #dont use, requires root
-                with uproot.recreate(outname) as file:
-                    file[args.treename] = pd.DataFrame(data['fj_pt'])
-
-                print('Wrote rootfile ', outname)
-
-                # you can further do hadd name_merged.root files*.root to merge the files per sample
+            print('Wrote rootfile ', outname)
