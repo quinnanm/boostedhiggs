39750ceec38107989b3a03ee85e206a01f5add4c
Farouk <farouk.mokhtar@gmail.com>
2022-06-03 17:33:46 +0200

    up

--- python/merge_root.sh
+++ python/merge_root.sh
@@ -6,12 +6,13 @@ mkdir -p roots
 
 # process the cms data
 for ch in rootfiles/* ; do
+  channel=name=$(awk -F'/' '{ a = length($NF) ? $NF : $(NF-1); print a }' <<< "$ch")
   for sample in $ch/* ; do
     cd $sample
     name=$(awk -F'/' '{ a = length($NF) ? $NF : $(NF-1); print a }' <<< "$sample")
     echo $name
-    hadd {$name}_{$ch}.root *
-    mv {$name}_{$ch}.root ../../../roots/
+    hadd $name_$channel.root *
+    mv $name_$channel.root ../../../roots/
     cd ../../..
   done
 done
