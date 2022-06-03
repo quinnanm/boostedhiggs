e6e45fb686836a3cae8f0df127e8a57e6238ba70
Farouk <farouk.mokhtar@gmail.com>
2022-06-03 15:25:32 +0200

    up

--- python/counting_from_pq.py
+++ python/counting_from_pq.py
@@ -34,7 +34,7 @@ warnings.filterwarnings("ignore", message="Found duplicate branch ")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--channels', dest='channels',  default='ele,mu,had',  help='channels for which to plot this variable')
-parser.add_argument('--dir',      dest='dir',       default='May7_2016',   help="tag for data directory")
+parser.add_argument('--dir',      dest='dir',       default='May7_2017',   help="tag for data directory")
 parser.add_argument('--odir',     dest='odir',      default='counts',       help="tag for output directory")
 
 args = parser.parse_args()
@@ -108,55 +108,6 @@ def compute_counts(channels, samples, odir):
         pkl.dump(num_dict, f)
 
 
-def make_pie(channels, odir):
-    """
-    Makes pie chart for a given channel
-    """
-
-    print(f'Making pie charts...')
-
-    with open(f'{odir}/num_dict.pkl', 'rb') as f:
-        num_dict = pkl.load(f)
-        f.close()
-
-    for ch in channels:
-        num_total = 0
-
-        for sample, num in num_dict[ch].items():
-            num_total = num_total + num
-
-        plot = {}
-        plot['others'] = 0
-        others = []
-
-        for sample, num in num_dict[ch].items():
-            if ('GluGluH' in sample):
-                plot['signal'] = 100 * num / num_total
-            elif (100 * num / num_total > 1):
-                plot[sample] = 100 * num / num_total
-            else:
-                plot['others'] = plot['others'] + 100 * num / num_total
-                others.append(sample)
-
-        col = []
-        for key in plot.keys():
-            col.append(color_dict[key])
-
-        fig, ax = plt.subplots(figsize=(9, 7))
-
-        patches, texts = ax.pie(plot.values(), colors=col, startangle=90, radius=1.2)
-        labels = ['{0} - {1:1.2f} %'.format(i, j) for i, j in zip(plot.keys(), plot.values())]
-
-        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-        ax.set_title(f'{ch} channel', size=40)
-        plt.legend(patches, labels, loc='upper left', bbox_to_anchor=(-0.1, 1.),
-                   fontsize=12)
-        plt.tight_layout()
-        plt.savefig(f'{odir}/pie_chart_{ch}.pdf')
-        plt.close()
-        print(f'others for {ch} ch include {others}')
-
-
 if __name__ == "__main__":
     """
     e.g. run locally as
@@ -180,4 +131,3 @@ if __name__ == "__main__":
         os.makedirs(args.odir)
 
     compute_counts(channels, samples, args.odir)
-    make_pie(channels, args.odir)
