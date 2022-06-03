460c50976051b5fdb35e9eb230c86e476da8212d
Farouk <farouk.mokhtar@gmail.com>
2022-06-03 17:19:38 +0200

    up

--- python/merge_root.sh
+++ python/merge_root.sh
@@ -6,7 +6,7 @@ set -e
 # process the cms data
 for ch in rootfiles/* ; do
   for sample in $ch/* ; do
-    cd sample
+    cd $sample
     name=$(awk -F'/' '{ a = length($NF) ? $NF : $(NF-1); print a }' <<< "$sample")
     hadd $name.root *
     mv $name.root ..
