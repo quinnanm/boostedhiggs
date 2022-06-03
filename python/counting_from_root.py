78f61ad4bb9c381ca233bb7cf07b2d27d79c746d
Farouk <farouk.mokhtar@gmail.com>
2022-06-03 18:22:57 +0200

    up

--- python/counting_from_root.py
+++ python/counting_from_root.py
@@ -9,26 +9,19 @@ import pickle as pkl
 
 # counts events stored in root files
 
-counts = {}
 
 for ch in ['had', 'mu', 'ele']:
-    counts[ch] = {}
+
     print(f'For {ch} channel')
 
     # repo = '/eos/uscms/store/user/mequinna/boostedhiggs/combinetest_23may22/merged/2017/'
     repo = '/uscms/home/fmokhtar/nobackup/boostedhiggs/python/merged/2017'
 
-    for sample in os.listdir(repo):
+    for merged_file in os.listdir(repo):
 
-        fname = repo + f'{sample}_{ch}_merged.root'
-
-        if os.path.isfile(f"{fname}"):
-            # load in uproot
-            events = uproot.open(f"{fname}")
-            # sum tot_weight
-            counts[ch][sample] = events['Events']['tot_weight'].array(library="np").sum()
-            print(f'number of events for {sample} is {num_dict[ch][sample]}')
+        # load in uproot
+        events = uproot.open(f"{merged_file}")
+        # sum tot_weight
+        counts = events['Events']['tot_weight'].array(library="np").sum()
+        print(f'number of events for {merged_file[:-12]} is {counts}')
     print(f'-----------------------------------------')
-
-with open(f'./counts_from_root.pkl', 'wb') as f:  # saves the hists objects
-    pkl.dump(counts, f)
--- python/merge_root.sh
+++ python/merge_root.sh
@@ -8,13 +8,13 @@ mkdir -p merged/2017
 
 # process the cms data
 for ch in rootfiles/* ; do
-  channel=$(awk -F'/' '{ a = length($NF) ? $NF : $(NF-1); print a }' <<< "$ch")
+  channel=$(awk -F'/' '{ a = length($NF) ? $NF : $(NF-1); print a }' <<< "$ch") # get channel as a variable
   for sample in $ch/* ; do
     cd $sample
-    name=$(awk -F'/' '{ a = length($NF) ? $NF : $(NF-1); print a }' <<< "$sample")
+    name=$(awk -F'/' '{ a = length($NF) ? $NF : $(NF-1); print a }' <<< "$sample")  # get sample as a variable
     echo $name
 
-    if [ "$(ls -A $DIR)" ]; then
+    if [ "$(ls -A $DIR)" ]; then  # skip if there were no parquets processed
      hadd $(echo ${name}_${channel})_merged.root *
      mv $(echo ${name}_${channel})_merged.root ../../../merged/2017/
     fi
