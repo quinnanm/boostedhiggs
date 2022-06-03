668e9071a0438fcd70eb28fe1b8c2afc8114eec4
Farouk <farouk.mokhtar@gmail.com>
2022-06-03 17:28:51 +0200

    d

--- python/merge_root.sh
+++ python/merge_root.sh
@@ -9,8 +9,8 @@ for ch in rootfiles/* ; do
   for sample in $ch/* ; do
     name=$(awk -F'/' '{ a = length($NF) ? $NF : $(NF-1); print a }' <<< "$sample")
     cd $sample
-    hadd $name_$ch.root *
-    mv $name_$ch.root ../../../roots/
+    hadd $name.root *
+    mv $name.root ../../../roots/
     cd ../../..
   done
 done
