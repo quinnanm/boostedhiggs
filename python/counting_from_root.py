766a875be77b6b8632153f86af2e83c27b3c32fd
Farouk <farouk.mokhtar@gmail.com>
2022-06-06 12:20:07 +0200

    up

--- python/counting_from_root.py
+++ python/counting_from_root.py
@@ -6,23 +6,37 @@ import numpy as np
 import pandas as pd
 import pyarrow.parquet as pq
 import pickle as pkl
+import argparse
 
 # counts events stored in root files
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--ch',       dest='ch',        default='ele,mu,had',  help='channels for which to plot this variable')
+parser.add_argument('--dir',      dest='dir',       default='./merged/2017',   help="tag for data directory")
 
-for ch in ['ele', 'mu', 'had']:
+args = parser.parse_args()
 
-    print(f'For {ch} channel')
+if __name__ == "__main__":
+    """
+    e.g. run locally as
+    python counting_from_root.py --dir merged/2017 --ch ele
+    """
 
-    # repo = '/eos/uscms/store/user/mequinna/boostedhiggs/combinetest_23may22/merged/2017/'
-    repo = '/uscms/home/fmokhtar/nobackup/boostedhiggs/python/merged/2017/'
+    channels = args.ch.split(',')
 
-    for merged_file in os.listdir(repo):
-        if ch not in merged_file:
-            continue
-        # load in uproot
-        events = uproot.open(f"{repo+merged_file}")
-        # sum tot_weight
-        counts = events['Events']['tot_weight'].array(library="np").sum()
-        print(f'number of events for {merged_file[:-12]} is {counts}')
-    print(f'-----------------------------------------')
+    for ch in channels:
+
+        print(f'For {ch} channel')
+
+        # repo = '/eos/uscms/store/user/mequinna/boostedhiggs/combinetest_23may22/merged/2017/'
+        # repo = '/uscms/home/fmokhtar/nobackup/boostedhiggs/python/merged/2017/'
+
+        for merged_file in os.listdir(args.dir):
+            if ch not in merged_file:
+                continue
+            # load in uproot
+            events = uproot.open(f"{args.dir+merged_file}")
+            # sum tot_weight
+            counts = events['Events']['tot_weight'].array(library="np").sum()
+            print(f'number of events for {merged_file[:-12]} is {counts}')
+        print(f'-----------------------------------------')
