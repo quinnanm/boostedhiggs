d0f31224cdc5e6eba7fc8f6e9776c42fa73b5459
Farouk <farouk.mokhtar@gmail.com>
2022-06-14 16:09:36 +0200

    up

--- boostedhiggs/TaggerInference.py
+++ boostedhiggs/TaggerInference.py
@@ -44,7 +44,7 @@ def get_pfcands_features(
         preselected_events[pfcands_label].jetIdx == jet_idx
     ]
     jet_pfcands = preselected_events.PFCands[jet_ak_pfcands.pFCandsIdx]
-
+    print('jet_pfcands', jet_pfcands)
     # get features
 
     # negative eta jets have -1 sign, positive eta jets have +1
