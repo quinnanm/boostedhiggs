6a1268b02988a60fc55a03199549d803d94f6ee7
Farouk <farouk.mokhtar@gmail.com>
2022-07-19 17:44:54 +0200

    up

--- python/s_over_b.py
+++ python/s_over_b.py
@@ -192,16 +192,18 @@ def plot_s_over_b(year, channels, odir, cut):
         ax.plot(wp[ch], num / np.sqrt(deno), label=f'{ch} channel, with s/b before cut = {str(round(legend,3))}')
 
     # ax.set_yscale('log')
-
-    # ax.set_title(r's/$\sqrt{b}$ as a function of the lepton isolation cut', fontsize=16)
-    # ax.set_title(r's/$\sqrt{b}$ as a function of the lepton mini-isolation cut', fontsize=16)
-    # ax.set_title(r's/$\sqrt{b}$ as a function of the dphi cut', fontsize=16)
-    ax.set_title('s/$\sqrt{b}$ as a function of the met_pt/lep_pt cut \n with DY, TTbar, Wjets background', fontsize=16)
-
-    # ax.set_xlabel('lep_iso<x|', fontsize=15)
-    # ax.set_xlabel('lep_miso<x|', fontsize=15)
-    # ax.set_xlabel('|dphi(met, jet)<x|', fontsize=15)
-    ax.set_xlabel(r'$\frac{pT_{met}}{pT_{lep}}$<x', fontsize=15)
+    if cut == 'met_lep':
+        ax.set_title('s/$\sqrt{b}$ as a function of the met_pt/lep_pt cut \n with DY, TTbar, Wjets background', fontsize=16)
+        ax.set_xlabel(r'$\frac{pT_{met}}{pT_{lep}}$<x', fontsize=15)
+    elif cut == 'dphi':
+        ax.set_title(r's/$\sqrt{b}$ as a function of the dphi cut', fontsize=16)
+        ax.set_xlabel('|dphi(met, jet)<x|', fontsize=15)
+    elif cut == 'iso':
+        ax.set_title(r's/$\sqrt{b}$ as a function of the lepton isolation cut', fontsize=16)
+        ax.set_xlabel('lep_iso<x|', fontsize=15)
+    elif cut == 'miso':
+        ax.set_title(r's/$\sqrt{b}$ as a function of the lepton mini-isolation cut', fontsize=16)
+        ax.set_xlabel('lep_miso<x|', fontsize=15)
 
     ax.set_ylabel(r's/$\sqrt{b}$', fontsize=15)
     ax.legend()
@@ -210,26 +212,26 @@ def plot_s_over_b(year, channels, odir, cut):
 
     # s/b for b=QCD
     fig, ax = plt.subplots(figsize=(8, 8))
-
     for ch in channels:
         num = counts[ch]['GluGluHToWWToLNuQQ']
         deno = counts[ch]['QCD']
-
         legend = s_over_b_before_cut[ch]['GluGluHToWWToLNuQQ'] / s_over_b_before_cut[ch]['QCD']
 
         ax.plot(wp[ch], num / np.sqrt(deno), label=f'{ch} channel, with s/b before cut = {str(round(legend,3))}')
 
     # ax.set_yscale('log')
-
-    # ax.set_title(r's/$\sqrt{b}$ as a function of the lepton isolation cut', fontsize=16)
-    # ax.set_title(r's/$\sqrt{b}$ as a function of the lepton mini-isolation cut', fontsize=16)
-    # ax.set_title(r's/$\sqrt{b}$ as a function of the dphi cut', fontsize=16)
-    ax.set_title('s/$\sqrt{b}$ as a function of the met_pt/lep_pt cut \n with QCD background', fontsize=16)
-
-    # ax.set_xlabel('lep_iso<x|', fontsize=15)
-    # ax.set_xlabel('lep_miso<x|', fontsize=15)
-    # ax.set_xlabel('|dphi(met, jet)<x|', fontsize=15)
-    ax.set_xlabel(r'$\frac{pT_{met}}{pT_{lep}}$<x', fontsize=15)
+    if cut == 'met_lep':
+        ax.set_title('s/$\sqrt{b}$ as a function of the met_pt/lep_pt cut \n with QCD background', fontsize=16)
+        ax.set_xlabel(r'$\frac{pT_{met}}{pT_{lep}}$<x', fontsize=15)
+    elif cut == 'dphi':
+        ax.set_title('s/$\sqrt{b}$ as a function of the dphi cut \n with QCD background', fontsize=16)
+        ax.set_xlabel('|dphi(met, jet)<x|', fontsize=15)
+    elif cut == 'iso':
+        ax.set_title('s/$\sqrt{b}$ as a function of the lepton isolation cut \n with QCD background', fontsize=16)
+        ax.set_xlabel('lep_iso<x|', fontsize=15)
+    elif cut == 'miso':
+        ax.set_title('s/$\sqrt{b}$ as a function of the lepton mini-isolation cut \n with QCD background', fontsize=16)
+        ax.set_xlabel('lep_miso<x|', fontsize=15)
 
     ax.set_ylabel(r's/$\sqrt{b}$', fontsize=15)
     ax.legend()
