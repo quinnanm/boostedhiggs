125dc2c6c9988aa6836468c25fd2045be657dc84
Farouk <farouk.mokhtar@gmail.com>
2022-07-26 20:26:06 +0200

    remove cuts for now

--- python/make_stacked_hists.py
+++ python/make_stacked_hists.py
@@ -114,10 +114,10 @@ def make_stacked_hists(year, ch, idir, odir, vars_to_plot, samples):
                         print('Parquet file empty')
                         continue
 
-                    if ch == 'ele':
-                        select = (data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]) | (data['lep_pt'] > max_iso[ch])
-                    elif ch == 'mu':
-                        select = ((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])) | ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))
+                    # if ch == 'ele':
+                    #     select = (data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]) | (data['lep_pt'] > max_iso[ch])
+                    # elif ch == 'mu':
+                    #     select = ((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])) | ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))
 
                     # filling histograms
                     single_sample = None
@@ -133,12 +133,16 @@ def make_stacked_hists(year, ch, idir, odir, vars_to_plot, samples):
 
                     # combining all pt bins of a specefic process under one name
                     # print(single_sample)
+                    # hists[var].fill(
+                    #     samples=sample_to_use,
+                    #     var=data[var][select],
+                    #     weight=event_weight[select],
+                    # )
                     hists[var].fill(
                         samples=sample_to_use,
-                        var=data[var][select],
-                        weight=event_weight[select],
+                        var=data[var],
+                        weight=event_weight,
                     )
-
                     # print(hists[var])
 
     # store the hists variable
