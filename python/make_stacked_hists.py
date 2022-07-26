04cf741477da098f324accec1fe7224224d9144f
Farouk <farouk.mokhtar@gmail.com>
2022-07-26 20:36:52 +0200

    up

--- python/make_stacked_hists.py
+++ python/make_stacked_hists.py
@@ -119,15 +119,15 @@ def make_stacked_hists(year, ch, idir, odir, vars_to_plot, samples):
                     # elif ch == 'mu':
                     #     select = ((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])) | ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))
 
-                    if ch == 'ele':
-                        select = (data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])
-                    elif ch == 'mu':
-                        select = ((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]))
-
                     # if ch == 'ele':
-                    #     select = (data['lep_pt'] > max_iso[ch])
+                    #     select = (data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])
                     # elif ch == 'mu':
-                    #     select = ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))
+                    #     select = ((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]))
+
+                    if ch == 'ele':
+                        select = (data['lep_pt'] > max_iso[ch])
+                    elif ch == 'mu':
+                        select = ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))
 
                     # filling histograms
                     single_sample = None
