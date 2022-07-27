ea031cc23360380303c1679bc523ea226eff95b5
Farouk <farouk.mokhtar@gmail.com>
2022-07-27 12:06:46 +0200

    up

--- python/make_stacked_hists.py
+++ python/make_stacked_hists.py
@@ -114,22 +114,25 @@ def make_stacked_hists(year, ch, idir, odir, vars_to_plot, samples):
                         print('Parquet file empty')
                         continue
 
-                    # if ch == 'ele':
-                    #     select = (data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]) | (data['lep_pt'] > max_iso[ch])
-                    # elif ch == 'mu':
-                    #     select = ((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])) | ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))
+                    if (var != 'lep_misolation') & (var != 'lep_isolation'):
+                        if ch == 'ele':
+                            select = (data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]) | (data['lep_pt'] > max_iso[ch])
+                        elif ch == 'mu':
+                            select = ((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])) | ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))
 
                     # for plotting miso
-                    if ch == 'ele':
-                        select = (((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]) | (data['lep_pt'] > max_iso[ch])) & (data['lep_pt'] > max_iso[ch])) & (data['lep_pt'] > max_iso[ch])
-                    elif ch == 'mu':
-                        select = (((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])) | ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))) & (data['lep_pt'] > max_iso[ch])
+                    if var == 'lep_misolation':
+                        if ch == 'ele':
+                            select = (((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]) | (data['lep_pt'] > max_iso[ch])) & (data['lep_pt'] > max_iso[ch])) & (data['lep_pt'] > max_iso[ch])
+                        elif ch == 'mu':
+                            select = (((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])) | ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))) & (data['lep_pt'] > max_iso[ch])
 
                     # # for plotting iso
-                    # if ch == 'ele':
-                    #     select = (((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]) | (data['lep_pt'] > max_iso[ch])) & (data['lep_pt'] < max_iso[ch])) & (data['lep_pt'] < max_iso[ch])
-                    # elif ch == 'mu':
-                    #     select = (((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])) | ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))) & (data['lep_pt'] < max_iso[ch])
+                    if var == 'lep_isolation':
+                        if ch == 'ele':
+                            select = (((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch]) | (data['lep_pt'] > max_iso[ch])) & (data['lep_pt'] < max_iso[ch])) & (data['lep_pt'] < max_iso[ch])
+                        elif ch == 'mu':
+                            select = (((data['lep_isolation'] < 0.15) & (data['lep_pt'] < max_iso[ch])) | ((data['lep_misolation'] < 0.1) & (data['lep_pt'] > max_iso[ch]))) & (data['lep_pt'] < max_iso[ch])
 
                     # filling histograms
                     single_sample = None
