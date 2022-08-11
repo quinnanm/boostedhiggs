de14e7a2c86d15f44ea3b956273a4d194606494c
Farouk <farouk.mokhtar@gmail.com>
2022-08-11 12:02:11 +0200

    add None accomodation for pnet_vars

--- boostedhiggs/hwwprocessor.py
+++ boostedhiggs/hwwprocessor.py
@@ -623,10 +623,13 @@ class HwwProcessor(processor.ProcessorABC):
                     )
                     print("post-inference")
                     print(pnet_vars)
-                    output[ch] = {
-                        **output[ch],
-                        **{key: value for (key, value) in pnet_vars.items()}
-                    }
+                    if pnet_vars == None:
+                        print('pnet_vars is None!!!... skipping storage')
+                    else:
+                        output[ch] = {
+                            **output[ch],
+                            **{key: value for (key, value) in pnet_vars.items()}
+                        }
             else:
                 output[ch] = {}
 
