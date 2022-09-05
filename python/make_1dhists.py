0b66ec58848d2271df8f24d43cc4e4a9c330376d
Farouk <farouk.mokhtar@gmail.com>
2022-09-05 10:47:07 +0200

    up

--- python/make_1dhists.py
+++ python/make_1dhists.py
@@ -185,6 +185,7 @@ if __name__ == "__main__":
     # lep_fj_dr: python make_1dhists.py --year 2017 --odir hists --channels ele --var lep_fj_dr --make_hists --plot_hists --bins 100 --start 0 --end 2 --idir /eos/uscms/store/user/cmantill/boostedhiggs/Sep2
     # fj_pt:     python make_1dhists.py --year 2017 --odir hists --channels had --var fj_pt     --make_hists --plot_hists --bins 100 --start 300 --end 1000 --idir /eos/uscms/store/user/cmantill/boostedhiggs/Sep2
     # gen_Hpt_pt:     python make_1dhists.py --year 2017 --odir test --channels ele --var gen_Hpt     --make_hists --plot_hists --bins 50 --start 300 --end 1000 --idir /eos/uscms/store/user/cmantill/boostedhiggs/Sep2
+    # gen_Hpt_pt:     python make_1dhists.py --year 2017 --odir test --channels ele --var fj_msoftdrop     --make_hists --plot_hists --bins 50 --start 25 --end 200 --idir /eos/uscms/store/user/cmantill/boostedhiggs/Sep2
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--year',            dest='year',        default='2017',                             help="year")
