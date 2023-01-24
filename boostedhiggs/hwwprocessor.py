66ea8c0b97ad8e9b1fa5845bd50ac0c5a2fddaa3
Cristina <criss.ms7@gmail.com>
2023-01-24 07:50:17 -0600

    revert changes of processor

--- boostedhiggs/hwwprocessor.py
+++ boostedhiggs/hwwprocessor.py
@@ -1,21 +1,21 @@
-import importlib.resources
-import json
-import os
-import pathlib
-import pickle as pkl
-import shutil
-import warnings
 from collections import defaultdict
-from typing import List, Optional
-
+import pickle as pkl
+import pyarrow as pa
 import awkward as ak
 import numpy as np
 import pandas as pd
-import pyarrow as pa
+import json
+import os
+import shutil
+import pathlib
+from typing import List, Optional
 import pyarrow.parquet as pq
+
+import importlib.resources
+
 from coffea import processor
-from coffea.analysis_tools import PackedSelection, Weights
 from coffea.nanoevents.methods import candidate, vector
+from coffea.analysis_tools import Weights, PackedSelection
 
 from boostedhiggs.utils import (
     match_H,
@@ -25,21 +25,17 @@ from boostedhiggs.utils import (
     pad_val,
 )
 from boostedhiggs.corrections import (
+    corrected_msoftdrop,
+    add_VJets_kFactors,
     add_jetTriggerSF,
     add_lepton_weight,
     add_pileup_weight,
-    add_VJets_kFactors,
-    corrected_msoftdrop,
 )
-from boostedhiggs.utils import (
-    get_neutrino_z,
-    match_H,
-    match_Top,
-    match_V,
-    pad_val,
-)
+from boostedhiggs.btag import btagWPs, BTagCorrector
 
 from .run_tagger_inference import runInferenceTriton
+
+import warnings
 
 warnings.filterwarnings("ignore", message="Found duplicate branch ")
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -211,7 +207,10 @@ class HwwProcessor(processor.ProcessorABC):
 
         # muons
         loose_muons = (
-            (((muons.pt > 30) & (muons.pfRelIso04_all < 0.25)) | (muons.pt > 55))
+            (
+                ((muons.pt > 30) & (muons.pfRelIso04_all < 0.25))
+                | (muons.pt > 55)
+            )
             & (np.abs(muons.eta) < 2.4)
             & (muons.looseId)
         )
@@ -234,7 +233,10 @@ class HwwProcessor(processor.ProcessorABC):
                 | (electrons.pt > 120)
             )
             & (np.abs(electrons.eta) < 2.4)
-            & ((np.abs(electrons.eta) < 1.44) | (np.abs(electrons.eta) > 1.57))
+            & (
+                (np.abs(electrons.eta) < 1.44)
+                | (np.abs(electrons.eta) > 1.57)
+            )
             & (electrons.cutBased >= electrons.LOOSE)
         )
         n_loose_electrons = ak.sum(loose_electrons, axis=1)
@@ -242,7 +244,10 @@ class HwwProcessor(processor.ProcessorABC):
         good_electrons = (
             (electrons.pt > 38)
             & (np.abs(electrons.eta) < 2.4)
-            & ((np.abs(electrons.eta) < 1.44) | (np.abs(electrons.eta) > 1.57))
+            & (
+                (np.abs(electrons.eta) < 1.44)
+                | (np.abs(electrons.eta) > 1.57)
+            )
             & (np.abs(electrons.dz) < 0.1)
             & (np.abs(electrons.dxy) < 0.05)
             & (electrons.sip3d <= 4.0)
@@ -421,11 +426,12 @@ class HwwProcessor(processor.ProcessorABC):
                 self.add_selection(name="signal", sel=signal_mask)
             elif ("WJets" in dataset) or ("ZJets" in dataset) or ("DYJets" in dataset):
                 genVars = match_V(events.GenPart, candidatefj)
-            elif "TT" in dataset:
+            elif ("TT" in dataset):
                 genVars = match_Top(events.GenPart, candidatefj)
             else:
                 genVars = {}
             variables["common"] = {**variables["common"], **genVars}
+
 
         """
         Weights
@@ -449,6 +455,7 @@ class HwwProcessor(processor.ProcessorABC):
         - LHE pdf weights for signal (ToDo) 
         - PSweights for signal (ToDo) 
         - ParticleNet tagger efficienc (ToDo) y
+
         Up and Down Variations (systematics included as a new variable)
         ----
         - Pileup weight Up/Down 
@@ -612,22 +619,20 @@ class HwwProcessor(processor.ProcessorABC):
 
                 # fill inference
                 if self.inference:
-                    for model_name in [
-                        "particlenet_hww_inclv2_pre2_noreg",
-                        "ak8_MD_vminclv2ParT_manual_fixwrap",
-                    ]:
+                    for model_name in ["particlenet_hww_inclv2_pre2_noreg",
+                                       "ak8_MD_vminclv2ParT_manual_fixwrap"]:
                         pnet_vars = runInferenceTriton(
                             self.tagger_resources_path,
                             events[selection_ch],
                             fj_idx_lep[selection_ch],
-                            model_name=model_name,
+                            model_name=model_name
                         )
-
+                        
                         output[ch] = {
                             **output[ch],
                             **{key: value for (key, value) in pnet_vars.items()},
                         }
-
+                        
             else:
                 output[ch] = {}
 
@@ -636,9 +641,7 @@ class HwwProcessor(processor.ProcessorABC):
                 output[ch] = self.ak_to_pandas(output[ch])
 
             if "rec_higgs_m" in output[ch].keys():
-                output[ch]["rec_higgs_m"] = np.nan_to_num(
-                    output[ch]["rec_higgs_m"], nan=-1
-                )
+                output[ch]["rec_higgs_m"] = np.nan_to_num(output[ch]["rec_higgs_m"], nan=-1)
 
         # now save pandas dataframes
         fname = events.behavior["__events_factory__"]._partition_key.replace("/", "_")
