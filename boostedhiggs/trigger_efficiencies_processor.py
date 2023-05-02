80991e797b9fe8fb514949e6e855bcd3cbc3dac4
Farouk <farouk.mokhtar@gmail.com>
2023-05-02 16:12:21 +0200

    up

--- boostedhiggs/trigger_efficiencies_processor.py
+++ boostedhiggs/trigger_efficiencies_processor.py
@@ -253,7 +253,7 @@ class TriggerEfficienciesProcessor(ProcessorABC):
 
             # use column accumulators
             for key_ in out[channel].keys():
-                out[channel] = {
+                out[channel][key_] = {
                     key: column_accumulator(value[selection.all(*selection.names)])
                     for (key, value) in out[channel][key_].items()
                 }
