b54a828827f59bbe4f3ecd644f0f0a3ab97f7ec5
Farouk <farouk.mokhtar@gmail.com>
2023-07-03 16:42:18 +0200

    up

--- condor/submit.templ.sh
+++ condor/submit.templ.sh
@@ -16,7 +16,17 @@ python SCRIPTNAME --year YEAR --processor PROCESSOR PFNANO INFERENCE SELECTION -
 rm -rf outfiles/*mu
 rm -rf outfiles/*ele
 
-# in cmsenv
+# setup CMSSW
+xrdcp -f root://cmseos.fnal.gov//store/user/cmantill/CMSSW_11_1_0_pre5_PY3.tgz ./CMSSW_11_1_0_pre5_PY3.tgz
+tar -zxvf CMSSW_11_1_0_pre5_PY3.tgz
+rm *.tgz
+cd CMSSW_*/src
+scram b ProjectRename
+eval `scramv1 runtime -sh`
+cmsenv
+cd ../../
+
+# merge rootfiles
 hadd outfiles/job_name.root outfiles/outroot/job_name/*
 rm -r outfiles/outroot
 
