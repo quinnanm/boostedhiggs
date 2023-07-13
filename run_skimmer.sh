1c247c8ef1304087787561af30c24ff9f4d2eee3
Farouk <farouk.mokhtar@gmail.com>
2023-07-13 18:20:17 +0200

    up

--- run_skimmer.sh
+++ run_skimmer.sh
@@ -3,10 +3,10 @@
 # run over signal files
 mkdir -p ntuples/GluGluHToWW_Pt-200ToInf_M-125/
 
-python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 1 --starti 0 --label H --inference
+python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 18 --starti 0 --label H --inference
 mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/train
 
-python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 1 --starti 1 --label H --inference
+python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 18 --starti 1 --label H --inference
 mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/test
 
 # # run over TTbar files
