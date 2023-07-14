99597c445c6ea04c7be9afb472ecac9468b183d1
Farouk <farouk.mokhtar@gmail.com>
2023-07-14 09:50:18 +0200

    qcd

--- run_hadd_on_simmer_output.sh
+++ run_hadd_on_simmer_output.sh
@@ -20,20 +20,8 @@
 # rm -r outroot
 # cd ../../../
 
-# merging WJetsLNu files
-for SAMPLE in WJetsToLNu_HT-400To600 WJetsToLNu_HT-600To800 WJetsToLNu_HT-800To1200
-do
-    cd ntuples/$SAMPLE/train
-    hadd -fk out.root outroot/*/*
-    rm -r outroot
-    cd ../test
-    hadd -fk out.root outroot/*/*
-    rm -r outroot
-    cd ../../../
-done
-
-# # merging QCD files
-# for SAMPLE in QCD_Pt_300to470 QCD_Pt_470to600 QCD_Pt_600to800 QCD_Pt_800to1000
+# # merging WJetsLNu files
+# for SAMPLE in WJetsToLNu_HT-200To400 WJetsToLNu_HT-400To600 WJetsToLNu_HT-600To800 WJetsToLNu_HT-800To1200
 # do
 #     cd ntuples/$SAMPLE/train
 #     hadd -fk out.root outroot/*/*
@@ -43,3 +31,15 @@ done
 #     rm -r outroot
 #     cd ../../../
 # done
+
+# merging QCD files
+for SAMPLE in QCD_Pt_300to470 QCD_Pt_470to600 QCD_Pt_600to800 QCD_Pt_800to1000
+do
+    cd ntuples/$SAMPLE/train
+    hadd -fk out.root outroot/*/*
+    rm -r outroot
+    cd ../test
+    hadd -fk out.root outroot/*/*
+    rm -r outroot
+    cd ../../../
+done
--- run_skimmer.sh
+++ run_skimmer.sh
@@ -18,26 +18,26 @@
 # python run.py --processor input --local --sample TTToSemiLeptonic --n 50 --starti 1 --label Top --inference
 # mv outfiles ntuples/TTToSemiLeptonic/test
 
-# run over WJetsLNu files
-for SAMPLE in WJetsToLNu_HT-200To400 WJetsToLNu_HT-400To600 WJetsToLNu_HT-600To800 WJetsToLNu_HT-800To1200
-do
-    mkdir -p ntuples/$SAMPLE/
-
-    python run.py --processor input --local --sample $SAMPLE --n 5 --starti 0 --label VJets --inference
-    mv outfiles ntuples/$SAMPLE/train
-
-    python run.py --processor input --local --sample $SAMPLE --n 5 --starti 1 --label VJets --inference
-    mv outfiles ntuples/$SAMPLE/test
-done
-
-# # run over QCD files
-# for SAMPLE in QCD_Pt_300to470 QCD_Pt_470to600 QCD_Pt_600to800 QCD_Pt_800to1000
+# # run over WJetsLNu files
+# for SAMPLE in WJetsToLNu_HT-200To400 WJetsToLNu_HT-400To600 WJetsToLNu_HT-600To800 WJetsToLNu_HT-800To1200
 # do
 #     mkdir -p ntuples/$SAMPLE/
 
-#     python run.py --processor input --local --sample $SAMPLE --n 5 --starti 0 --label QCD --inference
+#     python run.py --processor input --local --sample $SAMPLE --n 5 --starti 0 --label VJets --inference
 #     mv outfiles ntuples/$SAMPLE/train
 
-#     python run.py --processor input --local --sample $SAMPLE --n 5 --starti 1 --label QCD --inference
+#     python run.py --processor input --local --sample $SAMPLE --n 5 --starti 1 --label VJets --inference
 #     mv outfiles ntuples/$SAMPLE/test
 # done
+
+# run over QCD files
+for SAMPLE in QCD_Pt_300to470 QCD_Pt_470to600 QCD_Pt_600to800 QCD_Pt_800to1000
+do
+    mkdir -p ntuples/$SAMPLE/
+
+    python run.py --processor input --local --sample $SAMPLE --n 5 --starti 0 --label QCD --inference
+    mv outfiles ntuples/$SAMPLE/train
+
+    python run.py --processor input --local --sample $SAMPLE --n 5 --starti 1 --label QCD --inference
+    mv outfiles ntuples/$SAMPLE/test
+done
