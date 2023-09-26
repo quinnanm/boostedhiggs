110aac81634217ac2a6da0c69c4caa9a77657ace
Farouk <farouk.mokhtar@gmail.com>
2023-09-26 09:04:21 +0100

    up

--- run_skimmer.sh
+++ run_skimmer.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/bash
 
-# run over signal files
-mkdir -p ntuples/GluGluHToWW_Pt-200ToInf_M-125/
+# # run over signal samples
+# mkdir -p ntuples/GluGluHToWW_Pt-200ToInf_M-125/
 
-python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 10000 --starti 0 --label H --inference --year 2018
-mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/2018
+# python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 10000 --starti 0 --label H --inference --year 2018
+# mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/2018
 
-python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 10000 --starti 0 --label H --inference --year 2017
-mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/2017
+# python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 10000 --starti 0 --label H --inference --year 2017
+# mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/2017
 
-python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 10000 --starti 0 --label H --inference --year 2016APV
-mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/2016APV
+# python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 10000 --starti 0 --label H --inference --year 2016APV
+# mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/2016APV
 
-python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 10000 --starti 0 --label H --inference --year 2016
-mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/2016
+# python run.py --processor input --local --sample GluGluHToWW_Pt-200ToInf_M-125 --n 10000 --starti 0 --label H --inference --year 2016
+# mv outfiles ntuples/GluGluHToWW_Pt-200ToInf_M-125/2016
+
+# run over Top samples
+mkdir -p ntuples/TTToSemiLeptonic/
+
+python run.py --processor input --local --sample TTToSemiLeptonic --n 10000 --starti 0 --label Top --inference --year 2018
+mv outfiles ntuples/TTToSemiLeptonic/2018
+
+python run.py --processor input --local --sample TTToSemiLeptonic --n 10000 --starti 0 --label Top --inference --year 2017
+mv outfiles ntuples/TTToSemiLeptonic/2017
+
+python run.py --processor input --local --sample TTToSemiLeptonic --n 10000 --starti 0 --label Top --inference --year 2016APV
+mv outfiles ntuples/TTToSemiLeptonic/2016APV
+
+python run.py --processor input --local --sample TTToSemiLeptonic --n 10000 --starti 0 --label Top --inference --year 2016
+mv outfiles ntuples/TTToSemiLeptonic/2016
