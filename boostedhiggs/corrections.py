1655e238e4e6b362ceaa9bd257e7560fe55a2689
Farouk <farouk.mokhtar@gmail.com>
2024-05-22 09:26:54 +0200

    up

--- boostedhiggs/corrections.py
+++ boostedhiggs/corrections.py
@@ -1042,7 +1042,7 @@ def lep_removal(events, pt_array, eta_array, phi_array, mass_array, pid_array, G
 
     msk_lep = (pid_array == ELE_PDGID) | (pid_array == MU_PDGID) | (pid_array == TAU_PDGID)
 
-    msk = msk_lep & (delta_r < 0.1)
+    msk = msk_lep & (delta_r < 0.05)
     # msk = delta_r < 0.1
 
     pf_eta_rm_lep[msk] = 0.0
