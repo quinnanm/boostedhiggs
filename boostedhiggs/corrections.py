73276cc578a39a0f0e3811f9de431ed0955415a5
Farouk <farouk.mokhtar@gmail.com>
2024-05-22 09:30:07 +0200

    up

--- boostedhiggs/corrections.py
+++ boostedhiggs/corrections.py
@@ -1024,8 +1024,7 @@ def lep_removal(events, pt_array, eta_array, phi_array, mass_array, pid_array, G
     pf_phi = pad_val(phi_array, target=500, axis=1, value=0)
     pf_pt = pad_val(pt_array, target=500, axis=1, value=0)
     pf_mass = pad_val(mass_array, target=500, axis=1, value=0)
-
-    pid_array = pad_val(pid_array, target=500, axis=1, value=0)
+    pf_pid = pad_val(pid_array, target=500, axis=1, value=0)
 
     lep_eta_reshaped = lep_eta.reshape(-1, 1)
     lep_phi_reshaped = lep_phi.reshape(-1, 1)
@@ -1040,9 +1039,9 @@ def lep_removal(events, pt_array, eta_array, phi_array, mass_array, pid_array, G
     pf_pt_rm_lep = np.copy(pf_pt)
     pf_mass_rm_lep = np.copy(pf_mass)
 
-    msk_lep = (pid_array == ELE_PDGID) | (pid_array == MU_PDGID) | (pid_array == TAU_PDGID)
+    msk_lep = (pf_pid == ELE_PDGID) | (pf_pid == MU_PDGID) | (pf_pid == TAU_PDGID)
 
-    msk = msk_lep & (delta_r < 0.05)
+    msk = msk_lep & (delta_r < 0.0005)
     # msk = delta_r < 0.1
 
     pf_eta_rm_lep[msk] = 0.0
