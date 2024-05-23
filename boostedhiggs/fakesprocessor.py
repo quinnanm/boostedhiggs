f184f269d03cef59e790890a7449295e258c1d32
Farouk <farouk.mokhtar@gmail.com>
2024-05-23 17:11:36 +0200

    fake make it cut based electron

--- boostedhiggs/fakesprocessor.py
+++ boostedhiggs/fakesprocessor.py
@@ -203,7 +203,8 @@ class FakesProcessor(processor.ProcessorABC):
             (electrons.pt > 38)
             & (np.abs(electrons.eta) < 2.4)
             & ((np.abs(electrons.eta) < 1.44) | (np.abs(electrons.eta) > 1.57))
-            & (electrons.mvaFall17V2noIso_WPL)
+            # & (electrons.mvaFall17V2noIso_WPL)
+            & (electrons.cutBased >= electrons.LOOSE)
             & (((electrons.pfRelIso03_all < 0.25) & (electrons.pt < 120)) | (electrons.pt >= 120))
         )
 
