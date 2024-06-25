1de492694a6b4f982032cc24ad548354975710f7
Farouk <farouk.mokhtar@gmail.com>
2024-06-25 15:32:21 +0200

    up trg new

--- boostedhiggs/trigger_efficiencies_processor copy.py
+++ boostedhiggs/trigger_efficiencies_processor copy.py
@@ -168,7 +168,7 @@ class TriggerEfficienciesProcessor(ProcessorABC):
             (electrons.pt > 38)
             & (np.abs(electrons.eta) < 2.5)
             & ((np.abs(electrons.eta) < 1.44) | (np.abs(electrons.eta) > 1.57))
-            # & (electrons.mvaFall17V2noIso_WP90)
+            & (electrons.mvaFall17V2noIso_WP90)
             & (((electrons.pfRelIso03_all < 0.15) & (electrons.pt < 120)) | (electrons.pt >= 120))
             # additional cuts
             & (np.abs(electrons.dz) < 0.1)
@@ -265,9 +265,6 @@ class TriggerEfficienciesProcessor(ProcessorABC):
 
             out[channel]["vars"]["lep_pt"] = pad_val_nevents(candidatelep.pt)
             out[channel]["vars"]["lep_eta"] = pad_val_nevents(candidatelep.eta)
-
-            if channel == "ele":
-                out[channel]["vars"]["ele_MVA"] = pad_val_nevents(candidatelep.mvaFall17V2noIso_WP90)
 
             if "HToWW" in dataset:
                 genVars, _ = match_H(events.GenPart, candidatefj)
