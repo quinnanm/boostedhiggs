173618d611caf56efdd1577bd26e87af337833e5
Farouk <farouk.mokhtar@gmail.com>
2024-07-11 12:59:03 +0200

    up

--- lumi/compute_lumi.py
+++ lumi/compute_lumi.py
@@ -4,40 +4,25 @@ import numpy as np
 from coffea.lumi_tools import LumiData, LumiList
 
 """
-This script computes the total luminosity using a single lumi_set.pkl file
-and a lumi.csv produced using the GoldenJson.
+This script computes the total luminosity using
+    (1) a single lumi_set.pkl file saved using combine_lumi.py.
+    (2) a lumi.csv produced using the GoldenJson with the commands below.
+
+    # noqa for 2016: brilcalc lumi -c /cvmfs/cms.cern.ch/SITECONF/local/JobConfig/site-local-config.xml -b "STABLE BEAMS" --normtag=/cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -u /pb --byls --output-style csv -i ../boostedhiggs/data/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt > lumi2016.csv
+    # noqa for 2017: brilcalc lumi -c /cvmfs/cms.cern.ch/SITECONF/local/JobConfig/site-local-config.xml -b "STABLE BEAMS" --normtag=/cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -u /pb --byls --output-style csv -i ../boostedhiggs/data/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt > lumi2017.csv
+    # noqa for 2018: brilcalc lumi -c /cvmfs/cms.cern.ch/SITECONF/local/JobConfig/site-local-config.xml -b "STABLE BEAMS" --normtag=/cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -u /pb --byls --output-style csv -i ../boostedhiggs/data/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt > lumi2018.csv
 """
 
 
 def main():
-    # you can load the output!
-    year = "2016"
-    year_mod = "APV"
-    dir_ = f"/eos/uscms/store/user/fmokhtar/boostedhiggs/lumi_{year+year_mod}/"
+
+    year = "2016APV"
+    dir_ = f"/eos/uscms/store/user/fmokhtar/boostedhiggs/lumi_{year}/"
 
     import pickle
 
     with open(f"{dir_}/lumi_set.pkl", "rb") as f:
         lumi_set = pickle.load(f)
-
-    # # combine the sets from the different datasets
-    # for i, dataset in enumerate(lumi_set.keys()):
-    #     print(f"Retrieving lumi_set of {dataset}")
-    #     if i == 0:
-    #         lumis = lumi_set[dataset]
-    #     else:
-    #         lumis = lumis | lumi_set[dataset]
-
-    # # convert the set to a numpy 2d-array
-    # lumis = np.array(list(lumis))
-
-    # # make LumiList object
-    # lumi_list = LumiList(runs=lumis[:, 0], lumis=lumis[:, 1])
-
-    # # this csv was made using brilcalc and the GoldenJson2017... refer to
-    # https://github.com/CoffeaTeam/coffea/blob/52e102fce21a3e19f8c079adc649dfdd27c92075/coffea/lumi_tools/lumi_tools.py#L20
-    # lumidata = LumiData(f"lumi{year}.csv")
-    # print(f"---> Total Lumi = {lumidata.get_lumi(lumi_list)}")
 
     print("------------------------------------")
 
