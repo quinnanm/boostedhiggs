c69a2046dcbd8e737cf79909cf2fa5a2a9cb3d6e
Farouk <farouk.mokhtar@gmail.com>
2024-08-19 11:01:17 +0300

    up

--- boostedhiggs/trigger_efficiencies_processor.py
+++ boostedhiggs/trigger_efficiencies_processor.py
@@ -106,7 +106,7 @@ class TriggerEfficienciesProcessor(ProcessorABC):
             if t in events.HLT.fields:
                 trigger = trigger | events.HLT[t]
 
-        out[ch]["triggers"]["Muontrigger"] = trigger
+        out["ele"]["triggers"]["Muontrigger"] = np.array(trigger)
 
         ######################
         # METFLITERS
