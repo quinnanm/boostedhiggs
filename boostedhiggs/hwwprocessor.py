38cd65ef39d8d3068961c6551d3390da5b80d9cb
Cristina <criss.ms7@gmail.com>
2024-10-11 10:41:46 -0500

    revert ak4 jet veto

--- boostedhiggs/hwwprocessor.py
+++ boostedhiggs/hwwprocessor.py
@@ -357,16 +357,34 @@ class HwwProcessor(processor.ProcessorABC):
 
         ht = ak.sum(jets.pt, axis=1)
 
-        jet_selector = (
+        # AK4 jets with veto
+        # jet_selector = (
+        #     (jets.pt > 15)
+        #     & (abs(jets.eta) < 5.0)
+        #     & jets.isTight
+        #     & ((jets.pt >= 50) | ((jets.pt < 50) & (jets.puId & 2) == 2))
+        #     & (jets.chEmEF + jets.neEmEF < 0.9)  # neutral and charged energy fraction
+        # )
+        # jets = jets[jet_selector]
+        # jet_veto_map, _ = get_JetVetoMap(jets, self._year)
+        # jets = jets[(jets.pt > 30) & jet_veto_map]
+
+        loose_jet_selector = (
             (jets.pt > 15)
+            & jets.isTight
+            & ((jets.pt >= 50) | ((jets.pt < 50) & (jets.puId & 2) == 2))
+            & (jets.neEmEF < 0.9)  # neutral energy fraction
+        )
+        vetomapjets = jets[loose_jet_selector]
+        _, cut_jetveto = get_JetVetoMap(vetomapjets, self._year)
+        jet_selector = (
+            (jets.pt > 30)
             & (abs(jets.eta) < 5.0)
             & jets.isTight
             & ((jets.pt >= 50) | ((jets.pt < 50) & (jets.puId & 2) == 2))
-            & (jets.chEmEF + jets.neEmEF < 0.9)  # neutral and charged energy fraction
         )
-        jets = jets[jet_selector]
-        jet_veto_map, _ = get_JetVetoMap(jets, self._year)
-        jets = jets[(jets.pt > 30) & jet_veto_map]
+        goodjets = jets[jet_selector]
+
         ak4_outside_ak8_selector = jets.delta_r(candidatefj) > 0.8
         ak4_outside_ak8 = jets[ak4_outside_ak8_selector]
 
@@ -468,6 +486,8 @@ class HwwProcessor(processor.ProcessorABC):
             "VH_fj_pt": VH_fj.pt,
             "VH_fj_eta": VH_fj.eta,
             "VH_fj_VScore": VScore(VH_fj),
+            # add jetveto as optional selection
+            "jetvetomap": cut_jetveto,
             # added on October 9th
             "loose_lep1_miso": ak.firsts(
                 muons[loose_muons1][ak.argsort(muons[loose_muons1].pt, ascending=False)]
@@ -633,8 +653,9 @@ class HwwProcessor(processor.ProcessorABC):
 
         # hem-cleaning selection
         if self._year == "2018":
+            # TODO: change to only jets
             hem_veto = ak.any(
-                ((jets.eta > -3.2) & (jets.eta < -1.3) & (jets.phi > -1.57) & (jets.phi < -0.87)),
+                ((goodjets.eta > -3.2) & (goodjets.eta < -1.3) & (goodjets.phi > -1.57) & (goodjets.phi < -0.87)),
                 -1,
             ) | ak.any(
                 (
@@ -671,7 +692,9 @@ class HwwProcessor(processor.ProcessorABC):
                     nPU=ak.to_numpy(events.Pileup.nPU),
                 )
 
-                add_pileupid_weights(self.weights[ch], self._year, self._yearmod, jets, events.GenJet, wp="L")
+                # TODO: change to only jets
+                # add_pileupid_weights(self.weights[ch], self._year, self._yearmod, jets, events.GenJet, wp="L")
+                add_pileupid_weights(self.weights[ch], self._year, self._yearmod, goodjets, events.GenJet, wp="L")
 
                 if ch == "mu":
                     add_lepton_weight(self.weights[ch], candidatelep, self._year + self._yearmod, "muon")
