80d7c955cbbd7aac32947bd1063df2c03edbc488
Farouk <farouk.mokhtar@gmail.com>
2024-12-20 10:46:07 +0200

    allow condor submission of multiple years at once

--- condor/submit.py
+++ condor/submit.py
@@ -19,135 +19,139 @@ def main(args):
         print("No valid proxy. Exiting.")
         exit(1)
 
-    locdir = "condor/" + args.tag + "_" + args.year
-    username = os.environ["USER"]
-    homedir = f"/store/user/{username}/boostedhiggs/"
-    outdir = homedir + args.tag + "_" + args.year + "/"
+    years = args.years.split(",")
 
-    # make local directory
-    logdir = locdir + "/logs"
-    os.system(f"mkdir -p {logdir}")
+    for year in years:
 
-    # copy the splitting file to the locdir
-    os.system(f"cp pfnano_splitting.yaml {locdir}")
-    os.system(f"cp {args.config} {locdir}")
+        locdir = "condor/" + args.tag + "_" + year
+        username = os.environ["USER"]
+        homedir = f"/store/user/{username}/boostedhiggs/"
+        outdir = homedir + args.tag + "_" + year + "/"
 
-    # and condor directory
-    print("CONDOR work dir: " + outdir)
-    os.system(f"mkdir -p /eos/uscms/{outdir}")
+        # make local directory
+        logdir = locdir + "/logs"
+        os.system(f"mkdir -p {logdir}")
 
-    # build metadata.json with samples
-    slist = args.slist.split(",") if args.slist is not None else None
-    files, nfiles_per_job = loadFiles(args.config, args.configkey, args.year, args.pfnano, slist)
-    metadata_file = f"metadata_{args.configkey}.json"
-    with open(f"{locdir}/{metadata_file}", "w") as f:
-        json.dump(files, f, sort_keys=True, indent=2)
-    print(files.keys())
+        # copy the splitting file to the locdir
+        os.system(f"cp pfnano_splitting.yaml {locdir}")
+        os.system(f"cp {args.config} {locdir}")
 
-    # submit a cluster of jobs per sample
-    for sample in files.keys():
-        print(f"Making directory /eos/uscms/{outdir}/{sample}")
-        os.system(f"mkdir -p /eos/uscms/{outdir}/{sample}")
+        # and condor directory
+        print("CONDOR work dir: " + outdir)
+        os.system(f"mkdir -p /eos/uscms/{outdir}")
 
-        localcondor = f"{locdir}/{sample}.jdl"
-        localsh = f"{locdir}/{sample}.sh"
-        try:
-            os.remove(localcondor)
-            os.remove(localsh)
-            os.remove(f"{locdir}/*.log")
-        except Exception:
-            pass
+        # build metadata.json with samples
+        slist = args.slist.split(",") if args.slist is not None else None
+        files, nfiles_per_job = loadFiles(args.config, args.configkey, year, args.pfnano, slist)
+        metadata_file = f"metadata_{args.configkey}.json"
+        with open(f"{locdir}/{metadata_file}", "w") as f:
+            json.dump(files, f, sort_keys=True, indent=2)
+        print(files.keys())
 
-        tot_files = len(files[sample])
-        if (args.maxfiles != -1) & (args.maxfiles < tot_files):
-            tot_files = args.maxfiles
+        # submit a cluster of jobs per sample
+        for sample in files.keys():
+            print(f"Making directory /eos/uscms/{outdir}/{sample}")
+            os.system(f"mkdir -p /eos/uscms/{outdir}/{sample}")
 
-        if args.files_per_job:
-            njobs = ceil(tot_files / args.files_per_job)
-            files_per_job = str(args.files_per_job)
-        else:
-            njobs = ceil(tot_files / nfiles_per_job[sample])
-            files_per_job = str(nfiles_per_job[sample])
+            localcondor = f"{locdir}/{sample}.jdl"
+            localsh = f"{locdir}/{sample}.sh"
+            try:
+                os.remove(localcondor)
+                os.remove(localsh)
+                os.remove(f"{locdir}/*.log")
+            except Exception:
+                pass
 
-        # make submit.txt with number of jobs
-        if args.test:
-            njobs = 1
-        jobids = [str(jobid) for jobid in range(njobs)]
-        jobids_file = os.path.join(locdir, f"submit_{sample}.txt")
-        with open(jobids_file, "w") as f:
-            f.write("\n".join(jobids))
+            tot_files = len(files[sample])
+            if (args.maxfiles != -1) & (args.maxfiles < tot_files):
+                tot_files = args.maxfiles
 
-        # make condor file
-        condor_templ_file = open("condor/submit.templ.jdl")
-        condor_file = open(localcondor, "w")
-        for line in condor_templ_file:
-            line = line.replace("DIRECTORY", locdir)
-            line = line.replace("PREFIX", sample)
-            line = line.replace("JOBIDS_FILE", jobids_file)
-            line = line.replace("METADATAFILE", metadata_file)
-            line = line.replace("PROXY", proxy)
-            condor_file.write(line)
-        condor_file.close()
-        condor_templ_file.close()
-
-        # make executable file
-        sh_templ_file = open("condor/submit.templ.sh")
-        eosoutput_dir = f"root://cmseos.fnal.gov/{outdir}/{sample}/"
-        eosoutput_pkl = f"{eosoutput_dir}/"
-        sh_file = open(localsh, "w")
-        for line in sh_templ_file:
-            line = line.replace("SCRIPTNAME", args.script)
-            line = line.replace("YEAR", args.year)
-            line = line.replace("PROCESSOR", args.processor)
-            line = line.replace("METADATAFILE", metadata_file)
-            line = line.replace("NUMJOBS", files_per_job)
-            line = line.replace("SAMPLE", sample)
-            line = line.replace("CHANNELS", args.channels)
-            line = line.replace("EOSOUTPKL", eosoutput_pkl)
-            line = line.replace("PFNANO", f"--pfnano {args.pfnano}")
-            if args.inference:
-                line = line.replace("INFERENCE", "--inference")
+            if args.files_per_job:
+                njobs = ceil(tot_files / args.files_per_job)
+                files_per_job = str(args.files_per_job)
             else:
-                line = line.replace("INFERENCE", "--no-inference")
-            if args.systematics:
-                line = line.replace("SYSTEMATICS", "--systematics")
-            else:
-                line = line.replace("SYSTEMATICS", "--no-systematics")
-            if args.getLPweights:
-                line = line.replace("GETLPWEIGHTS", "--getLPweights")
-            else:
-                line = line.replace("GETLPWEIGHTS", "--no-getLPweights")
+                njobs = ceil(tot_files / nfiles_per_job[sample])
+                files_per_job = str(nfiles_per_job[sample])
 
-            if args.uselooselep:
-                line = line.replace("LOOSELEP", "--uselooselep")
-            else:
-                line = line.replace("LOOSELEP", "--no-uselooselep")
+            # make submit.txt with number of jobs
+            if args.test:
+                njobs = 1
+            jobids = [str(jobid) for jobid in range(njobs)]
+            jobids_file = os.path.join(locdir, f"submit_{sample}.txt")
+            with open(jobids_file, "w") as f:
+                f.write("\n".join(jobids))
 
-            line = line.replace("LABEL", args.label)
-            line = line.replace("REGION", args.region)
+            # make condor file
+            condor_templ_file = open("condor/submit.templ.jdl")
+            condor_file = open(localcondor, "w")
+            for line in condor_templ_file:
+                line = line.replace("DIRECTORY", locdir)
+                line = line.replace("PREFIX", sample)
+                line = line.replace("JOBIDS_FILE", jobids_file)
+                line = line.replace("METADATAFILE", metadata_file)
+                line = line.replace("PROXY", proxy)
+                condor_file.write(line)
+            condor_file.close()
+            condor_templ_file.close()
 
-            sh_file.write(line)
-        sh_file.close()
-        sh_templ_file.close()
+            # make executable file
+            sh_templ_file = open("condor/submit.templ.sh")
+            eosoutput_dir = f"root://cmseos.fnal.gov/{outdir}/{sample}/"
+            eosoutput_pkl = f"{eosoutput_dir}/"
+            sh_file = open(localsh, "w")
+            for line in sh_templ_file:
+                line = line.replace("SCRIPTNAME", args.script)
+                line = line.replace("YEAR", year)
+                line = line.replace("PROCESSOR", args.processor)
+                line = line.replace("METADATAFILE", metadata_file)
+                line = line.replace("NUMJOBS", files_per_job)
+                line = line.replace("SAMPLE", sample)
+                line = line.replace("CHANNELS", args.channels)
+                line = line.replace("EOSOUTPKL", eosoutput_pkl)
+                line = line.replace("PFNANO", f"--pfnano {args.pfnano}")
+                if args.inference:
+                    line = line.replace("INFERENCE", "--inference")
+                else:
+                    line = line.replace("INFERENCE", "--no-inference")
+                if args.systematics:
+                    line = line.replace("SYSTEMATICS", "--systematics")
+                else:
+                    line = line.replace("SYSTEMATICS", "--no-systematics")
+                if args.getLPweights:
+                    line = line.replace("GETLPWEIGHTS", "--getLPweights")
+                else:
+                    line = line.replace("GETLPWEIGHTS", "--no-getLPweights")
 
-        os.system(f"chmod u+x {localsh}")
-        if os.path.exists("%s.log" % localcondor):
-            os.system("rm %s.log" % localcondor)
+                if args.uselooselep:
+                    line = line.replace("LOOSELEP", "--uselooselep")
+                else:
+                    line = line.replace("LOOSELEP", "--no-uselooselep")
 
-        # submit
-        if args.submit:
-            print("Submit ", localcondor)
-            os.system("condor_submit %s" % localcondor)
+                line = line.replace("LABEL", args.label)
+                line = line.replace("REGION", args.region)
+
+                sh_file.write(line)
+            sh_file.close()
+            sh_templ_file.close()
+
+            os.system(f"chmod u+x {localsh}")
+            if os.path.exists("%s.log" % localcondor):
+                os.system("rm %s.log" % localcondor)
+
+            # submit
+            if args.submit:
+                print("Submit ", localcondor)
+                os.system("condor_submit %s" % localcondor)
 
 
 if __name__ == "__main__":
     """
-    python condor/submit.py --year 2017 --tag test --config samples_inclusive.yaml --key mc --no-inference --channels mu
+    # noqa: python condor/submit.py --years 2018,2017,2016,2016APV --tag test --config samples_inclusive.yaml --key mc --no-inference --channels mu,ele
     """
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--script", dest="script", default="run.py", help="script to run", type=str)
-    parser.add_argument("--year", dest="year", default="2017", help="year", type=str)
+    parser.add_argument("--years", dest="years", required=True, help="years separated by commas")
     parser.add_argument("--tag", dest="tag", default="Test", help="process tag", type=str)
     parser.add_argument(
         "--processor",
